8ee82e350d5e984cba6a54a147fce9a0800fc6d7
nikolaims <smtnm@ya.ru>
2016-04-22 12:08:19 +0300

    eeg stable

--- pynfb/lsl/visualizers.py
+++ pynfb/lsl/visualizers.py
@@ -1,67 +1,80 @@
-import time
+from pyqtgraph.Qt import QtGui, QtCore
+from pylsl import StreamInlet, resolve_stream
 import numpy as np
-from pylsl import StreamInfo, StreamOutlet, StreamInlet, resolve_stream
+import pyqtgraph as pg
+import sys
+import time
+
+pg.setConfigOptions(antialias=False)
+app = QtGui.QApplication([])
+win = pg.GraphicsWindow(title="pyqtgraph example: Linked Views")
+win.resize(800, 600)
+win.addLabel("Linked Views", colspan=2)
+n_plots = 15
+n_samples = 1000
+data = np.zeros((n_samples, n_plots))
+
+curves = []
+p_old = None
+p = None
+for j in range(n_plots):
+    win.nextRow()
+    p_new = win.addPlot(y=data[j])
+    if j == 0: p = p_new
+    if p_old:  p_new.setXLink(p_old)
+    if j < n_plots - 1: p_new.showAxis('bottom', show=False)
+    c = pg.PlotCurveItem(pen=(j, n_plots * 1.3))
+    p_old = p_new
+    p_old.addItem(c)
+    curves.append(c)
+
+streams = resolve_stream('name', ['AudioCaptureWin','NVX136_Data', 'example'][1])  # 'AudioCaptureWin')
+inlet = StreamInlet(streams[0], max_buflen=1, max_chunklen=8)
+t0 = time.time()
+t = t0
+c = 1
+chunk_counter = 0
+zero_counter = 0
+none_counter = 0
+freq = 120
 
 
-def run_eeg_inlet(n_channels=30, freq=500):
-    streams = resolve_stream('name', 'example')  # 'AudioCaptureWin')
-    # create a new inlet to read from the stream
-    inlet = StreamInlet(streams[0], max_buflen=1)
-    t0 = time.time()
-    t = t0
-    c = 1
-    while True:
-        sample, timestamp = inlet.pull_sample()
-        if c % (freq * 5) == 0:
-            t_curr = time.time()
-            print('t={:.1f}, f={:.2f}, sample.shape={}'.format(t_curr - t0,
-                                                               1. / (t_curr - t) * freq * 5,
-                                                               np.array(sample).shape))
-            t = t_curr
-        c += 1
-    pass
+def update():
+    global chunk_counter, zero_counter, none_counter, c, t, t0, data
+    chunk, timestamp = inlet.pull_chunk()
+    chunk = chunk
+    max_samples = len(chunk)
+    if max_samples>0:
+        data[:-max_samples] = data[max_samples:]
+        data[-max_samples:] = np.array(chunk)[:, :n_plots]
 
+        for i in range(c%1, n_plots, 1):
+            curves[i].setData(data[:, i])
+    if chunk_counter is None:
+        none_counter += 1
+    if len(chunk) > 0:
+        if len(chunk) > 8:
+            print(len(chunk))
+            pass
 
+        chunk_counter += 1
+    else:
+        zero_counter += 0
+    # if len(chunk) > 0:
+    if c % (freq) == 0:
+        t_curr = time.time()
+        print('t={:.1f}, f={:.2f}, chunks={}, zeros={}, nones={}'.format(t_curr - t0,
+                                                                         1. / (t_curr - t) * freq,
+                                                                         chunk_counter, zero_counter, none_counter))
+        t = t_curr
+        chunk_counter = 0
+        zero_counter = 0
+        none_counter = 0
+    c += 1
 
-def simple():
-    print('blip blop')
-    pass
-
-
-def run_eeg_inlet_chunks(action=simple, n_channels=30, freq=120):
-    streams = resolve_stream('name', 'example')  # 'AudioCaptureWin')
-    # create a new inlet to read from the stream
-    inlet = StreamInlet(streams[0], max_buflen=1, max_chunklen=8)
-    t0 = time.time()
-    t = t0
-    c = 1
-    chunk_counter = 0
-    zero_counter = 0
-    none_counter = 0
-    while True:
-        time.sleep(1. / freq)
-        chunk, timestamp = inlet.pull_chunk()
-        action()
-        if chunk_counter is None:
-            none_counter += 1
-        if len(chunk)>0:
-            if len(chunk)!=8:
-                print(len(chunk))
-            chunk_counter += 1
-        else:
-            zero_counter += 0
-        #if len(chunk) > 0:
-        if c % (freq) == 0:
-            t_curr = time.time()
-            print('t={:.1f}, f={:.2f}, chunks={}, zeros={}, nones={}'.format(t_curr - t0,
-                                                         1. / (t_curr - t) * freq,
-                                                         chunk_counter, zero_counter, none_counter))
-            t = t_curr
-            chunk_counter = 0
-            zero_counter = 0
-            none_counter = 0
-        c += 1
-    pass
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(1000*1./freq)
 
 if __name__ == '__main__':
-    run_eeg_inlet_chunks()
+    sys.exit(app.exec_())
