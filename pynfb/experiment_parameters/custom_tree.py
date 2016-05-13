ada2211d4f660950e95e37149b654c3bbce3d39f
Nikolay <smtnm@ya.ru>
2016-05-13 17:02:01 +0300

    custom parameters

--- pynfb/experiment_parameters/custom_tree.py
+++ pynfb/experiment_parameters/custom_tree.py
@@ -1,75 +1,84 @@
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtGui
+from collections import OrderedDict
 
-data = [
-    ("Alice", [
-        ("Keys", []),
-        ("Purse", [
-            ("Cellphone", [])
-            ])
-        ]),
-    ("Bob", [
-        ("Wallet", [
-            ("Credit card", []),
-            ("Money", [])
-            ])
-        ])
-    ]
+signals = [{'sSignalName': 'Signal1',
+             'fBandpassLowHz': 1,
+             'fBandpassHighHz': 10},
+            {'sSignalName': 'Signal2',
+             'fBandpassLowHz': 1,
+             'fBandpassHighHz': 30}
+            ]
 
-class Window(QWidget):
 
-    def __init__(self):
+class SignalsList(QtGui.QWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        label = QtGui.QLabel('Signals:')
+        self.list = QtGui.QListWidget(self)
 
-        QWidget.__init__(self)
-
-        self.treeView = QTreeView()
-        self.treeView.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.treeView.customContextMenuRequested.connect(self.openMenu)
-
-        self.model = QStandardItemModel()
-        self.addItems(self.model, data)
-        self.treeView.setModel(self.model)
-
-        self.model.setHorizontalHeaderLabels([self.tr("Object")])
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.treeView)
+        self.set_data()
+        self.list.itemDoubleClicked.connect(self.item_double_clicked_event)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.list)
         self.setLayout(layout)
+        self.show()
 
-    def addItems(self, parent, elements):
+    def item_double_clicked_event(self, item):
+        self.signals_dialogs[self.list.currentRow()].open()
 
-        for text, children in elements:
-            item = QStandardItem(text)
-            parent.appendRow(item)
-            if children:
-                self.addItems(item, children)
+    def set_data(self):
+        self.list.clear()
+        self.signals_dialogs = []
+        for signal in signals:
+            item = QtGui.QListWidgetItem(signal['sSignalName'])
 
-    def openMenu(self, position):
+            self.signals_dialogs.append(SignalDialog(self, signal_name=signal['sSignalName']))
+            self.list.addItem(item)
 
-        indexes = self.treeView.selectedIndexes()
-        if len(indexes) > 0:
+class SignalDialog(QtGui.QDialog):
+    def __init__(self, parent, signal_name='Signal'):
+        super().__init__(parent)
+        self.parent_list = parent
+        self.setWindowTitle('Properties: '+signal_name)
+        self.form_layout = QtGui.QFormLayout(self)
+        # name
+        self.name = QtGui.QLineEdit(self)
+        self.name.setText(signal_name)
+        self.form_layout.addRow('&Name:', self.name)
+        # bandpass
+        self.bandpass_low = QtGui.QSpinBox()
+        self.bandpass_low.setRange(0, 250)
+        self.bandpass_low.setValue(0)
+        self.form_layout.addRow('&Bandpass low [Hz]:', self.bandpass_low)
+        self.bandpass_high = QtGui.QSpinBox()
+        self.bandpass_high.setRange(0, 250)
+        self.bandpass_high.setValue(250)
+        self.form_layout.addRow('&Bandpass high [Hz]:', self.bandpass_high)
+        # ok button
+        self.save_button = QtGui.QPushButton('Save')
+        self.save_button.clicked.connect(self.save_and_close)
+        self.form_layout.addRow(self.save_button)
 
-            level = 0
-            index = indexes[0]
-            while index.parent().isValid():
-                index = index.parent()
-                level += 1
+    def open(self):
+        self.set_data()
+        super().open()
 
-        menu = QMenu()
-        if level == 0:
-            menu.addAction(self.tr("Edit person"))
-        elif level == 1:
-            menu.addAction(self.tr("Edit object/container"))
-        elif level == 2:
-            menu.addAction(self.tr("Edit object"))
+    def set_data(self):
+        print(self.parent().list.currentRow())
+        self.bandpass_low.setValue(signals[self.parent().list.currentRow()]['fBandpassLowHz'])
+        self.bandpass_high.setValue(signals[self.parent().list.currentRow()]['fBandpassHighHz'])
 
-        menu.exec_(self.treeView.viewport().mapToGlobal(position))
+    def save_and_close(self):
+        signals[self.parent().list.currentRow()]['sSignalName'] = self.name.text()
+        self.parent().set_data()
+        self.close()
 
 
 if __name__ == "__main__":
 
-    app = QApplication(sys.argv)
-    window = Window()
+    app = QtGui.QApplication(sys.argv)
+    window = SignalsList()
     window.show()
     sys.exit(app.exec_())
