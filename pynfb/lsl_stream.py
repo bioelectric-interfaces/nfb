2ced8415e1dc449bfaaadd92aa420e46ae2c445e
Nikolay <smtnm@ya.ru>
2016-06-02 17:59:36 +0300

    set np.array dtype from lsl info

--- pynfb/lsl_stream.py
+++ pynfb/lsl_stream.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from pylsl import StreamInlet, resolve_byprop
+fmt2string = ['undefined', 'float32', 'float64', 'str', 'int32', 'int16',
+              'int8', 'int64']
 
 
 LSL_STREAM_NAMES = ['AudioCaptureWin', 'NVX136_Data', 'example']
@@ -10,15 +12,18 @@ class LSLStream():
     def __init__(self, name=LSL_STREAM_NAMES[2], max_chunklen=8):
         streams = resolve_byprop('name', name, timeout=LSL_RESOLVE_TIMEOUT)
         self.inlet = None
+        self.dtype = 'float64'
         if len(streams) > 0:
             self.inlet = StreamInlet(streams[0], max_buflen=1, max_chunklen=max_chunklen)
+            self.dtype = fmt2string[self.inlet.info().channel_format()]
+            print(self.dtype)
 
 
     def get_next_chunk(self):
         # get next chunk
         chunk, timestamp = self.inlet.pull_chunk()
         # convert to numpy array
-        chunk = np.array(chunk)
+        chunk = np.array(chunk, dtype=self.dtype)
         # return first n_channels channels or None if empty chunk
         return chunk if chunk.shape[0] > 0 else None
 
