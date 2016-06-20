c440da00dd0c89056096f002b6dc04c79303d798
nikolaims <smtnm@ya.ru>
2016-06-20 19:25:46 +0300

    topomap correct map: ch name to location

---
+++ pynfb/protocols/custom_filter_estimator/__init__.py
@@ -0,0 +1,12 @@
+from os import system
+
+if __name__ == '__main__':
+    with open('example.py', 'r') as f:
+        get_filter_code = f.read()
+
+    run_get_filter_code = ("\nfrom pynfb.io.hdf5 import load_h5py \ndata = 'test'\nfilter = get_filter(data)\nprint('filter saved', filter)")
+
+    with open('_run_get_filter.py', 'w') as f:
+        f.write(get_filter_code + run_get_filter_code)
+
+    system("\"E:\_nikolai\programs\WinPython\python-3.4.4.amd64\python\" _run_get_filter.py")
---
+++ pynfb/protocols/custom_filter_estimator/example.py
@@ -0,0 +1,12 @@
+# This is an example file with function "get_filter"
+from numpy import array
+
+
+def get_filter(data):
+    """
+    This function estimates filter for recorded data
+    :param data: recorded data
+    :return: _filter (numpy array or list)
+    """
+    _filter = array([])
+    return _filter
