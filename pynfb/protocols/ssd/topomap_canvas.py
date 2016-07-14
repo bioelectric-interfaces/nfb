352911ebe6b6d7dbc2063ced9f0fd7e788599eb7
Nikolay <smtnm@ya.ru>
2016-07-14 17:03:30 +0300

    spatial filter setup widget

--- pynfb/protocols/ssd/topomap_canvas.py
+++ pynfb/protocols/ssd/topomap_canvas.py
@@ -1,8 +1,11 @@
 import sys
 from PyQt4 import QtGui, QtCore
 import numpy as np
+from pynfb.widgets.helpers import ch_names_to_2d_pos
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+from matplotlib import rcParams
+rcParams['font.size'] = 8
 try:
     from mne.viz import plot_topomap
 except ImportError:
@@ -10,29 +13,42 @@ except ImportError:
 
 
 class TopographicMapCanvas(FigureCanvas):
-    def __init__(self, data, pos, names=None, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, data, pos=None, names=None, parent=None, width=5, height=4, dpi=100, show_names=None):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
         self.colorbar = None
         FigureCanvas.__init__(self, self.fig)
-        self.update_figure(data, pos, names)
+        self.update_figure(data, pos, names, show_names=show_names)
         self.setParent(parent)
         FigureCanvas.setSizePolicy(self,
                                    QtGui.QSizePolicy.Expanding,
                                    QtGui.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
-    def update_figure(self, data, pos, names=None):
+    def update_figure(self, data, pos=None, names=None, show_names=None):
+        if pos is None:
+            pos = ch_names_to_2d_pos(names)
+        data = np.array(data)
         self.axes.clear()
         if self.colorbar:
             self.colorbar.remove()
-        if names:
-            mask = np.array([name.upper() in ['O1', 'O2', 'CZ', 'T3', 'T4', 'T7', 'T8', 'FP1', 'FP2'] for name in names])
-        else:
-            mask = None
+        if show_names is None:
+            show_names = ['O1', 'O2', 'CZ', 'T3', 'T4', 'T7', 'T8', 'FP1', 'FP2']
+        show_names = [name.upper() for name in show_names]
+        mask = np.array([name.upper() in show_names for name in names]) if names else None
+        v_min, v_max = None, None
+        if (data == data[0]).all():
+            data += np.random.uniform(-1e-3, 1e-3, size=len(data))
+            v_min, v_max = -1, 1
         a, b = plot_topomap(data, pos, axes=self.axes, show=False, contours=0, names=names, show_names=True,
                             mask=mask,
-                            mask_params=dict(marker='o', markerfacecolor='k', markeredgecolor='w', linewidth=0, markersize=3))
+                            mask_params=dict(marker='o',
+                                             markerfacecolor='w',
+                                             markeredgecolor='w',
+                                             linewidth=0,
+                                             markersize=3),
+                            vmin=v_min,
+                            vmax=v_max)
         self.colorbar = self.fig.colorbar(a, orientation='horizontal', ax = self.axes)
         self.colorbar.ax.tick_params(labelsize=6)
         self.colorbar.ax.set_xticklabels(self.colorbar.ax.get_xticklabels(), rotation=90)
---
+++ pynfb/widgets/spatial_filter_setup.py
@@ -0,0 +1,105 @@
+from PyQt4 import QtGui, QtCore
+import sys
+from pynfb.protocols.ssd.topomap_canvas import TopographicMapCanvas
+
+
+class Table(QtGui.QTableWidget):
+    def __init__(self, ch_names, *args):
+        super(Table, self).__init__(*args)
+
+        # set size and names
+        self.setColumnCount(2)
+        self.setRowCount(len(ch_names))
+        self.setHorizontalHeaderLabels(['Channel', 'Weight'])
+
+        # set ch names
+        for ind, name in enumerate(ch_names):
+            name_item = QtGui.QTableWidgetItem(name)
+            name_item.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.setItem(ind, 0, name_item)
+
+        # set spin boxes and default weights
+        self.weights = [0 for _j in range(self.rowCount())]
+        for ind, w in enumerate(self.weights):
+            spin_box = QtGui.QDoubleSpinBox()
+            spin_box.setValue(w)
+            spin_box.setSingleStep(0.5)
+            spin_box.setRange(-1e5, 1e5)
+            self.setCellWidget(ind, 1, spin_box)
+
+        # formatting
+        self.horizontalHeader().setStretchLastSection(True)
+        self.verticalHeader().setVisible(False)
+
+    def set_weights(self, weights):
+        self.weights = weights
+        for ind, w in enumerate(weights):
+            self.cellWidget(ind, 1).setValue(w)
+
+    def revert_changes(self):
+        self.set_weights(self.weights)
+
+    def commit_changes(self):
+        self.weights = [self.cellWidget(ind, 1).value() for ind in range(self.rowCount())]
+
+    def get_weights(self):
+        return self.weights
+
+
+class SpatialFilterSetup(QtGui.QDialog):
+    def __init__(self, ch_names, weights=None, **kwargs):
+        super(SpatialFilterSetup, self).__init__(**kwargs)
+        #
+        self.ch_names = ch_names
+        self.weights = weights if weights is not None else [0 for _j in self.ch_names]
+
+        # layout
+        layout = QtGui.QGridLayout(self)
+
+        # table
+        self.table = Table(ch_names)
+        if weights is not None:
+            self.table.set_weights(weights)
+        layout.addWidget(self.table, 0, 1)
+
+        # topomap canvas
+        self.topomap = TopographicMapCanvas(weights, names=ch_names, show_names=ch_names)
+        layout.addWidget(self.topomap, 0, 0)
+
+        # buttons
+        btn_layout = QtGui.QHBoxLayout()
+        apply_btn = QtGui.QPushButton('Apply')
+        apply_btn.clicked.connect(self.apply_action)
+        revert_btn = QtGui.QPushButton('Revert')
+        revert_btn.clicked.connect(self.table.revert_changes)
+        ok_btn = QtGui.QPushButton('OK')
+        ok_btn.clicked.connect(self.ok_action)
+        btn_layout.addWidget(revert_btn)
+        btn_layout.addWidget(apply_btn)
+        btn_layout.addWidget(ok_btn)
+        layout.addLayout(btn_layout, 1, 1)
+
+    def apply_action(self):
+        self.table.commit_changes()
+        self.topomap.update_figure(self.table.get_weights(), names=self.ch_names, show_names=self.ch_names)
+
+    def ok_action(self):
+        self.table.commit_changes()
+        self.weights = self.table.get_weights()
+        self.close()
+
+    @staticmethod
+    def get_filter(ch_names, weights=None):
+        selector = SpatialFilterSetup(ch_names, weights=weights)
+        _result = selector.exec_()
+        return selector.weights
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    app = QtGui.QApplication([])
+    ch_names = ['Fc1', 'Fc3', 'Fc5', 'C1', 'C3', 'C5', 'Cp1', 'Cp3', 'Cp5', 'Cz', 'Pz',
+                'Cp2', 'Cp4', 'Cp6', 'C2', 'C4', 'C6', 'Fc2', 'Fc4', 'Fc6']
+    w = SpatialFilterSetup.get_filter(ch_names, np.random.uniform(size=len(ch_names)))
+    print(w)
