composite scaling fix

pynfb/signals/__init__.py
@@ -103,7 +103,7 @@ class DerivedSignal():
         self.std_acc = self.var_acc ** 0.5
         self.n_acc += chunk_size
 
-        if self.scaling_flag:
+        if self.scaling_flag and self.std > 0:
             self.current_sample = (self.current_sample - self.mean) / self.std
         pass
 
@@ -114,7 +114,13 @@ class DerivedSignal():
         amplitude = np.abs(cut_f_signal).mean()
         return amplitude
 
-    def update_statistics(self, mean=None, std=None):
+    def update_statistics(self, mean=None, std=None, raw=None, emulate=False):
+        if raw is not None and emulate:
+            self.reset_statistic_acc()
+            mean_chunk_size = 8
+            for k in range(0, raw.shape[0] - mean_chunk_size, mean_chunk_size):
+                chunk = raw[k:k + mean_chunk_size]
+                self.update(chunk)
         self.mean = mean if (mean is not None) else self.mean_acc
         self.std = std if (std is not None) else self.std_acc
         self.reset_statistic_acc()