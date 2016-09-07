2753313b0273b8fc488b12d21b1cbf8ea0bf7122
nikolaims <smtnm@ya.ru>
2016-09-07 15:05:52 +0300

    ica ui check full sell

--- pynfb/protocols/ssd/topomap_selector_ica.py
+++ pynfb/protocols/ssd/topomap_selector_ica.py
@@ -2,7 +2,6 @@ from PyQt4 import QtGui, QtCore
 from pynfb.protocols.ssd.topomap_canvas import TopographicMapCanvas
 from pyqtgraph import PlotWidget
 from pyqtgraph import setConfigOption
-setConfigOption('leftButtonPan', False)
 from mne import create_info
 from mne.io import RawArray
 from mne.channels import read_montage
@@ -10,24 +9,28 @@ from mne.preprocessing import ICA
 import numpy as np
 from sklearn.metrics import mutual_info_score
 
+
 def mutual_info(x, y, bins=100):
     c_xy = np.histogram2d(x, y, bins)[0]
     mi = mutual_info_score(None, None, contingency=c_xy)
     return mi
+
 
 class Table(QtGui.QTableWidget):
     one_selected = QtCore.pyqtSignal()
     more_one_selected = QtCore.pyqtSignal()
     no_one_selected = QtCore.pyqtSignal()
 
-    def __init__(self, time_series, topographies, channel_names, fs, scores,*args):
+    def __init__(self, time_series, topographies, channel_names, fs, scores, *args):
+        super(Table, self).__init__(*args)
+
+        # attributes
         self.row_items_max_height = 125
         self.time_series = time_series
         self.topographies = topographies
         self.channel_names = channel_names
         self.fs = fs
 
-        super(Table, self).__init__(*args)
         # set size and names
         self.columns = ['Selection', 'Mutual info', 'Topography', 'Time series (push to switch mode)']
         self.setColumnCount(len(self.columns))
@@ -62,7 +65,7 @@ class Table(QtGui.QTableWidget):
                 plot_widget.setXLink(_previous_plot_link)
                 plot_widget.setYLink(_previous_plot_link)
             _previous_plot_link = plot_widget
-            plot_widget.plot(x=np.arange(self.time_series.shape[0])/fs)
+            plot_widget.plot(x=np.arange(self.time_series.shape[0]) / fs)
             plot_widget.plot(y=self.time_series[:, ind])
             plot_widget.setMaximumHeight(self.row_items_max_height)
             plot_widget.plotItem.getViewBox().state['wheelScaleFactor'] = 0
@@ -80,8 +83,8 @@ class Table(QtGui.QTableWidget):
         self.resizeColumnsToContents()
         self.resizeRowsToContents()
 
-        # clickable 3 column
-        self.horizontalHeader().sectionClicked.connect(self.changeHorizontalHeader)
+        # clickable 3 column header
+        self.horizontalHeader().sectionClicked.connect(self.handle_header_click)
         self.is_spectrum_mode = False
 
         # reorder
@@ -92,19 +95,37 @@ class Table(QtGui.QTableWidget):
         for checkbox in self.checkboxes:
             checkbox.stateChanged.connect(self.checkboxes_state_changed)
 
+        # selection context menu
         header = self.horizontalHeader()
         header.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        header.customContextMenuRequested.connect(self.handleHeaderMenu)
+        header.customContextMenuRequested.connect(self.handle_header_menu)
+
+        # ctrl+a short cut
+        self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_A), self),
+                     QtCore.SIGNAL('activated()'), self.ctrl_plus_a_event)
+
+        # checkbox cell clicked
+        self.cellClicked.connect(self.cell_was_clicked)
+
+    def cell_was_clicked(self, row, column):
+        if column == 0:
+            self.checkboxes[self.order[row]].click()
+
+    def ctrl_plus_a_event(self):
+        if len(self.get_unchecked_rows()) == 0:
+            self.clear_selection()
+        else:
+            self.select_all()
 
     def contextMenuEvent(self, pos):
         if self.columnAt(pos.x()) == 0:
-            self.open_checkbox_menu()
+            self.open_selection_menu()
 
-    def handleHeaderMenu(self, pos):
+    def handle_header_menu(self, pos):
         if self.horizontalHeader().logicalIndexAt(pos) == 0:
-            self.open_checkbox_menu()
+            self.open_selection_menu()
 
-    def open_checkbox_menu(self):
+    def open_selection_menu(self):
         menu = QtGui.QMenu()
         for name, method in zip(['Revert selection', 'Select all', 'Clear selection'],
                                 [self.revert_selection, self.select_all, self.clear_selection]):
@@ -134,7 +155,7 @@ class Table(QtGui.QTableWidget):
         else:
             self.more_one_selected.emit()
 
-    def changeHorizontalHeader(self, index):
+    def handle_header_click(self, index):
         if index == 3:
             self.set_spectrum_mode(flag=not self.is_spectrum_mode)
         if index == self.columns.index('Mutual info'):
@@ -147,12 +168,12 @@ class Table(QtGui.QTableWidget):
             if flag:
                 window = int(4 * self.fs)
                 if len(y) >= window:
-                    y = np.mean([np.abs(np.fft.fft(y[j:j + window])**2 / y.shape[0])
-                                  for j in range(0, y.shape[0] - window, window//2)], axis=0)
+                    y = np.mean([np.abs(np.fft.fft(y[j:j + window]) ** 2 / y.shape[0])
+                                 for j in range(0, y.shape[0] - window, window // 2)], axis=0)
                 else:
-                    y = np.abs(np.fft.fft(y) / y.shape[0])**2
-                x = np.fft.fftfreq(window, d=1/self.fs)
-                plot_item.plot(x=x[:y.shape[0]//2], y=y[:y.shape[0]//2], clear=True)
+                    y = np.abs(np.fft.fft(y) / y.shape[0]) ** 2
+                x = np.fft.fftfreq(window, d=1 / self.fs)
+                plot_item.plot(x=x[:y.shape[0] // 2], y=y[:y.shape[0] // 2], clear=True)
                 self.columns[-1] = 'Spectrum'
             else:
                 plot_item.plot(x=np.arange(self.time_series.shape[0]) / self.fs, y=y, clear=True)
@@ -177,16 +198,14 @@ class Table(QtGui.QTableWidget):
             self.setCellWidget(ind, 3, self.plot_items[new_ind])
             self.setCellWidget(ind, 1, self.scores[new_ind])
             self.setRowHeight(ind, self.row_items_max_height)
-        for ind in range(self.rowCount()//2):
-            self.removeRow(self.rowCount()-1)
+        for ind in range(self.rowCount() // 2):
+            self.removeRow(self.rowCount() - 1)
 
     def set_scores(self, scores):
         for j, score in enumerate(self.scores):
             score.setText(str(scores[j]))
         self.order = np.argsort(scores)
         self.reorder()
-
-
 
 
 class ICADialog(QtGui.QDialog):
@@ -255,8 +274,7 @@ class ICADialog(QtGui.QDialog):
         self.reject_button.clicked.connect(self.reject_and_close)
         self.spatial_button.clicked.connect(self.spatial_and_close)
 
-
-        #self.sort_button.clicked.connect(lambda : self.table.reorder())
+        # self.sort_button.clicked.connect(lambda : self.table.reorder())
 
         # layout
         layout = QtGui.QVBoxLayout(self)
@@ -269,7 +287,7 @@ class ICADialog(QtGui.QDialog):
         layout.addLayout(buttons_layout)
 
         # enable maximize btn
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowMaximizeButtonHint )
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowMaximizeButtonHint)
 
         # checkboxes behavior
         self.table.no_one_selected.connect(lambda: self.reject_button.setDisabled(True))
@@ -278,17 +296,8 @@ class ICADialog(QtGui.QDialog):
         self.table.one_selected.connect(lambda: self.spatial_button.setDisabled(False))
         self.table.more_one_selected.connect(lambda: self.reject_button.setDisabled(False))
         self.table.more_one_selected.connect(lambda: self.spatial_button.setDisabled(True))
+
         self.table.checkboxes_state_changed()
-
-        # ctrl+a short cut
-        self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_A), self),
-                     QtCore.SIGNAL('activated()'), self.down)
-
-    def down(self):
-        if len(self.table.get_unchecked_rows()) == 0:
-            self.table.clear_selection()
-        else:
-            self.table.select_all()
 
     def sort_by_mutual(self):
         ind = self.sort_combo.currentIndex()
@@ -312,17 +321,20 @@ class ICADialog(QtGui.QDialog):
         result = selector.exec_()
         return selector.rejection, selector.ica
 
+
 if __name__ == '__main__':
     import numpy as np
+
     app = QtGui.QApplication([])
     n_channels = 3
     fs = 100
     from pynfb.widgets.helpers import ch_names_to_2d_pos
+
     channels = ['Cp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'Ft9', 'Fc5', 'Fc1', 'Fc2', 'Fc6', 'Ft10', 'T7', 'C3', 'Cz',
                 'C4', 'T8', 'Tp9', 'Cp5', 'Cp1', 'Cp2', 'Cp6', 'Tp10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'O1', 'Oz', 'O2']
     channels = channels[:n_channels]
 
-    x = np.array([np.sin(10*(f+1)*2*np.pi*np.arange(0, 10, 1/fs)) for f in range(n_channels)]).T
+    x = np.array([np.sin(10 * (f + 1) * 2 * np.pi * np.arange(0, 10, 1 / fs)) for f in range(n_channels)]).T
 
     # Generate sample data
     np.random.seed(0)
@@ -332,6 +344,7 @@ if __name__ == '__main__':
     s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
     s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
     from scipy import signal
+
     s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: saw tooth signal
 
     S = np.c_[s1, s2, s3]
