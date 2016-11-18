79877207c8e0c4a657d6141b5f882fd720bfe65e
nikolaims <smtnm@ya.ru>
2016-11-18 12:33:27 +0300

    cross

--- pynfb/widgets/signals_painter.py
+++ pynfb/widgets/signals_painter.py
@@ -3,40 +3,35 @@ from PyQt4 import QtGui, QtCore
 
 
 class CrossButtonsWidget(QtGui.QWidget):
-    left_pressed = QtCore.pyqtSignal()
-    right_pressed = QtCore.pyqtSignal()
-    top_pressed = QtCore.pyqtSignal()
-    bottom_pressed = QtCore.pyqtSignal()
+    names = ['left', 'right', 'up', 'down']
+    symbols = ['<', '>', '^', 'v']
+    positions = [(1, 0), (1, 2), (0, 1), (2, 1)]
 
     def __init__(self, parent=None):
         super(CrossButtonsWidget, self).__init__(parent)
-        left = QtGui.QPushButton('<')
-        left.clicked.connect(lambda : self.left_pressed.emit())
-        right = QtGui.QPushButton('>')
-        right.clicked.connect(lambda: self.right_pressed.emit())
-        top = QtGui.QPushButton('^')
-        top.clicked.connect(lambda: self.top_pressed.emit())
-        bottom = QtGui.QPushButton('v')
-        bottom.clicked.connect(lambda: self.bottom_pressed.emit())
-
+        buttons = dict([(name, QtGui.QPushButton(symbol)) for name, symbol in zip(self.names, self.symbols)])
         layout = QtGui.QGridLayout(self)
-        layout.addWidget(left, 1, 0)
-        layout.addWidget(right, 1, 2)
-        layout.addWidget(top, 0, 1)
-        layout.addWidget(bottom, 2, 1)
-
-a = QtGui.QApplication([])
-plot_widget = pg.PlotWidget()
-
-btn = CrossButtonsWidget()
-
-#btn_item = QtGui.QGraphicsProxyWidget()
-#btn_item.setWidget(btn)
-#plot_widget.addItem(btn_item)
-btn.setParent(plot_widget)
-btn.left_pressed.connect(lambda: print('LEFT!'))
-btn.setGeometry(50, 0, 100, 100)
-plot_widget.show()
+        self.clicked_dict = {}
+        for name, pos in zip(self.names, self.positions):
+            layout.addWidget(buttons[name], *pos)
+            self.clicked_dict[name] = buttons[name].clicked
 
 
-a.exec_()
+
+if __name__ == '__main__':
+    a = QtGui.QApplication([])
+    plot_widget = pg.PlotWidget()
+
+    btn = CrossButtonsWidget()
+
+    #btn_item = QtGui.QGraphicsProxyWidget()
+    #btn_item.setWidget(btn)
+    #plot_widget.addItem(btn_item)
+    btn.setParent(plot_widget)
+    btn.clicked_dict['left'].connect(lambda: print('LEFT!'))
+    btn.clicked_dict['down'].connect(lambda: print('DOWN!'))
+    btn.clicked_dict['up'].connect(lambda: print('UP!'))
+    btn.clicked_dict['right'].connect(lambda: print('R!'))
+    btn.setGeometry(50, 0, 100, 100)
+    plot_widget.show()
+    a.exec_()
