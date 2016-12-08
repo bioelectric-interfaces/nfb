add92a86cd837fecc3eb417a44bc67a24d1b2d53
nikolaims <smtnm@ya.ru>
2016-12-08 17:25:58 +0300

    postprocessing results signals

--- pynfb/postprocessing/plot_all_fb_bars.py
+++ pynfb/postprocessing/plot_all_fb_bars.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import seaborn
 import pandas as pd
-experiment_file = 'C:\\Users\\Nikolai\\Downloads\\pilot\\pilot_Emelyannikov29_1_11-09_20-14-28\\experiment_data.h5'
+experiment_file = 'C:\\Users\\Nikolai\\Downloads\\pilot\\pilot_Nikolay_2_10-18_14-57-23\\experiment_data.h5'
 
 with h5py.File(experiment_file) as f:
     root = ET.fromstring(f['settings.xml'][:][0])
@@ -14,12 +14,23 @@ with h5py.File(experiment_file) as f:
     composite = [x.find('sSignalName').text for x in root.find('vSignals').findall('CompositeSignal')]
 
     data_to_plot = pd.DataFrame(columns=['val', 'signal', 'protocol'])
+
+    prev = lambda x: 'protocol' + str(int(x.replace('protocol', '')) - 1)
+    val0 = {}
     for k, fb_key in enumerate(fb_keys):
-        for signal in derived:
-            data_to_plot = data_to_plot.append(pd.DataFrame({'val': [f['{}/signals_stats/{}/mean'.format(fb_key, signal)].value],
-                                'signal': signal,
-                                'protocol': fb_key}
-                                               ))
+        for j, signal in enumerate(derived):
+            mean = f['{}/signals_stats/{}/mean'.format(prev(fb_key), signal)].value
+            std = f['{}/signals_stats/{}/std'.format(prev(fb_key), signal)].value
+            signal_data = f['{}/signals_data'.format(fb_key)][:][:, j]
+            print(signal_data)
+            print(signal_data.mean(), signal_data.std())
+            val = np.mean(signal_data * std + mean)
+            if k == 0:
+                val0[signal] = val
+            data_to_plot = data_to_plot.append(
+                pd.DataFrame({'val': [val / val0[signal]],
+                              'signal': signal,
+                              'protocol': fb_key}))
 
 seaborn.barplot(data=data_to_plot, x='protocol', y='val', hue='signal')
 seaborn.plt.show()
