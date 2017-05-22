f4e607fff787f5b496d4bf9f2adf1aaa352eb955
nsmetanin <smtnm@ya.ru>
2017-05-22 17:57:51 +0300

    expyriment stim

--- tests/psy_tests/qt_plus_expyriment.py
+++ tests/psy_tests/qt_plus_expyriment.py
@@ -1,15 +1,39 @@
 import expyriment
 
-exp = expyriment.design.Experiment(name="First Experiment")
-expyriment.io.Screen(window_mode=True)
-expyriment.control.initialize(exp)
+from expyriment import control, design, misc
+import expyriment.stimuli
+from PyQt4 import QtGui, QtCore
 
-stim = expyriment.stimuli.TextLine(text="Hello World")
-stim.preload()
-expyriment.control.defaults.window_mode = True
-expyriment.control.start(skip_ready_screen=True, subject_id=1)
+def run_exp():
+    control.run_test_suite()
+    control.set_develop_mode(True)
+    control.defaults.initialize_delay = 0
+    garbor = expyriment.stimuli.Rectangle((100, 100)) # Picture('image.png')
+    cross = expyriment.stimuli.Rectangle((100, 100), position=(100, 100))
+    exp = design.Experiment(background_colour=(0, 0, 0))
+    control.initialize(exp)
 
-stim.present()
-exp.clock.wait(1000)
 
-expyriment.control.end()
+    garbor.preload()
+    time = 0
+    mean = 0
+    wait_time = 0
+    for k in range(1, 1000):
+        t = garbor.present()
+        cross.present(clear=False)
+        exp.clock.wait(500)
+        #expyriment.stimuli.Rectangle((100, 100)).present(clear=False)
+        t += expyriment.stimuli.BlankScreen().present()
+        #
+        mean += t
+        print(t, mean / k)
+        exp.clock.wait(500)
+    control.end()
+
+
+app = QtGui.QApplication([])
+w = QtGui.QPushButton('Run exp')
+w.show() #showFullScreen()
+w.clicked.connect(run_exp)
+app.exec_()
+
