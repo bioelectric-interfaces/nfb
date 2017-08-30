da0d70289d47f503a6add64905a97397dda6bdaa
nsmetanin <smtnm@ya.ru>
2017-08-30 17:03:30 +0300

    filters: smoothers, band detectors

--- pynfb/signal_processing/filters.py
+++ pynfb/signal_processing/filters.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, savgol_coeffs
+from scipy.fftpack import rfft, irfft, fftfreq
 from  scipy import fftpack
 
 class BaseFilter:
@@ -132,17 +133,177 @@ class Coherence(BaseFilter):
             np.dot(H[:, 0], H[:, 0].conj()) * np.dot(H[:, 1], H[:, 1].conj())))
         return np.ones(len(chunk)) * np.abs(np.imag(coh))
 
+
+class ExponentialSmoother(BaseFilter):
+    def __init__(self, factor):
+        self.a = [1, -factor]
+        self.b = [1 - factor]
+        self.zi = np.zeros((max(len(self.a), len(self.b)) - 1, ))
+
+    def apply(self, chunk: np.ndarray):
+        y, self.zi = lfilter(self.b, self.a, chunk, zi=self.zi)
+        return y
+
+class SGSmoother(BaseFilter):
+    def __init__(self, n_samples, sg_order):
+        self.savgol_weights = savgol_coeffs(n_samples, sg_order, pos=n_samples - 1)
+        self.b, self.a = (self.savgol_weights, [1.])
+        self.zi = np.zeros((n_samples - 1, ))
+
+    def apply(self, chunk: np.ndarray):
+        y, self.zi = lfilter(self.b, self.a, chunk, zi=self.zi)
+        return y
+
+
+class FFTBandEnvelopeDetector(BaseFilter):
+    def __init__(self, band, fs, factor, n_samples):
+        # bandpass filter settings
+        self.buffer = np.zeros((n_samples,))
+        self.n_samples = n_samples
+        self.w = fftfreq(2 * n_samples, d=1. / fs * 2)
+        self.band = (band[0] or 0, band[1] or fs/2)
+
+        # asymmetric gaussian window
+        p = round(2 * n_samples * 2 / 4)  # maximum
+        eps = 0.0001  # bounds value
+        power = 2  # power of x
+        left_c = - np.log(eps) / (p ** power)
+        right_c = - np.log(eps) / (2 * n_samples - 1 - p) ** power
+        samples_window = np.concatenate([np.exp(-left_c * abs(np.arange(p) - p) ** power),
+                                         np.exp(-right_c * abs(np.arange(p, 2 * n_samples) - p) ** power)])
+        self.samples_window = samples_window
+
+        # exponential smoothing
+        self.exp_smoother = ExponentialSmoother(factor)
+
+    def apply(self, chunk: np.ndarray):
+        # update buffer
+        chunk_size = chunk.shape[0]
+        self.chunk_size = chunk_size
+        if chunk_size <= self.n_samples:
+            self.buffer[:-chunk_size] = self.buffer[chunk_size:]
+            self.buffer[-chunk_size:] = chunk
+        else:
+            self.buffer = chunk[-self.n_samples:]
+
+        # bandpass filter and amplitude
+        f_signal = rfft(np.hstack((self.buffer, self.buffer[-1::-1])) * self.samples_window)
+        cut_f_signal = f_signal.copy()
+        cut_f_signal[(self.w < self.band[0]) | (self.w > self.band[1])] = 0  # TODO: in one row
+        y = np.ones_like(chunk) * np.abs(cut_f_signal).mean()
+
+        # smoothing
+        y = self.exp_smoother.apply(y)
+        return y
+
+class ButterBandEnvelopeDetector(BaseFilter):
+    def __init__(self, band, fs, factor, order=4):
+        self.butter_filter = ButterFilter(band, fs, 1, order=order)
+        self.exp_smoother = ExponentialSmoother(factor)
+
+    def apply(self, chunk: np.ndarray):
+        y = self.butter_filter.apply(chunk[:, None])[:, 0]
+        y = np.abs(y)
+        y = self.exp_smoother.apply(y)
+        return y
+
+
+class SGBandEnvelopeDetector(BaseFilter):
+    def __init__(self, band, fs, factor, n_samples, sg_order):
+        self.exp_smoother = ExponentialSmoother(factor)
+        self.buffer = np.zeros((n_samples,))
+        self.n_samples = n_samples
+        self.band = band
+        # step 1: demodulation
+        main_fq = (self.band[0] + self.band[1]) / 2
+        #print(source_freq, main_fq)
+        self.modulation = np.exp(-2j * np.pi * np.arange(1000 * fs / main_fq) / fs * main_fq)
+        self.n_modulation_samples = len(self.modulation)
+        self.modulation_timer = len(self.modulation) - 1
+        # step 2: iir
+        self.iir_b, self.iir_a = butter(1, (self.band[1] - self.band[0]) / fs)
+        self.zf = [0]
+        # step 3: sav gol
+        self.savgol_weights = savgol_coeffs(self.n_samples, sg_order, pos=self.n_samples - 1)
+        self.sg_b, self.sg_a = (self.savgol_weights, [1.])
+        self.sg_zf = np.zeros((n_samples - 1, ))
+
+    def apply(self, chunk: np.ndarray):
+        # update buffer
+        chunk_size = chunk.shape[0]
+        self.chunk_size = chunk_size
+        if chunk_size <= self.n_samples:
+            self.buffer[:-chunk_size] = self.buffer[chunk_size:]
+            self.buffer[-chunk_size:] = chunk
+        else:
+            self.buffer = chunk[-self.n_samples:]
+
+        # bandpass filter and amplitude
+        self.modulation_timer += chunk_size
+        starting_index = (self.modulation_timer - self.chunk_size) % self.n_modulation_samples
+        ending_index = self.modulation_timer % self.n_modulation_samples
+        if starting_index > ending_index:
+            part1 = self.modulation[starting_index:]
+            part2 = self.modulation[:ending_index]
+            result = np.concatenate([part1, part2])
+        else:
+            result = self.modulation[starting_index:ending_index]
+        x = result * chunk
+        # print(np.concatenate([[self.iir_buffer], x]))
+        y, self.zf = lfilter(self.iir_b, self.iir_a, x, zi=self.zf)
+        y, self.sg_zf = lfilter(self.sg_b, self.sg_a, y, zi=self.sg_zf)
+        #y = self.exp_smoother.apply(y)
+        y = np.ones_like(chunk) * np.abs(2 * y)
+        return y
+
 if __name__ == '__main__':
     import pylab as plt
 
     print('TEST: Butter Filter')
     n_samples = 10000
     time = np.arange(n_samples)/250
-    data = np.sin(10 * 2 * np.pi * time.repeat(2).reshape(n_samples, 2)) + np.random.normal(size=(n_samples, 1))*0.1
+    data = np.sin(10 * 2 * np.pi * time.repeat(2).reshape(n_samples, 2))
+    noise = np.random.normal(size=(n_samples, 1))*0.1
     butter_filter = ButterFilter((9, None), fs=250, n_channels=1)
     #plt.plot(time, data)
     #plt.plot(time, butter_filter.apply(data))
-    plt.plot(np.vstack([butter_filter.apply(data[k*200:(k+1)*200]) for k in range(n_samples//200)]), '--')
-    butter_filter.reset()
-    plt.plot(np.vstack([butter_filter.apply(data[k * 8:(k + 1) * 8]) for k in range(n_samples // 8)]), '--')
+    #plt.plot(np.vstack([butter_filter.apply(data[k*200:(k+1)*200]) for k in range(n_samples//200)]), '--')
+    #butter_filter.reset()
+    #plt.plot(np.vstack([butter_filter.apply(data[k * 8:(k + 1) * 8]) for k in range(n_samples // 8)]), '--')
+
+
+    print('TEST: Exp Smoother')
+    data = np.sin(10 * 2 * np.pi * time)
+
+    butter_filter = ExponentialSmoother(0.9)
+    #plt.plot(data)
+    #data = data + noise
+    #plt.plot(data)
+    #plt.plot(np.vstack([butter_filter.apply(data[k * 200:(k + 1) * 200]) for k in range(n_samples // 200)]), '--')
+
+
+    print('TEST: butter env det')
+    data = data + noise[:, 0]
+    data[n_samples//2:] *= 0.5
+    print(data.shape)
+    butter_filter = ButterBandEnvelopeDetector((9, 12), 250, 0.99, 1)
+    plt.plot(data)
+    res = np.hstack([butter_filter.apply(data[k * 8:(k + 1) * 8]) for k in range(n_samples // 8)])
+    plt.plot(1.55*res)
+    #plt.show()
+
+    print('TEST: fft env det')
+    #data = data + noise
+    #data[n_samples//2:] *= 0.5
+    butter_filter = FFTBandEnvelopeDetector((9, 12), 250, 0.96, 500)
+    #plt.plot(data)
+    plt.plot(1.45*np.hstack([butter_filter.apply(data[k * 8:(k + 1) * 8]) for k in range(n_samples // 8)]))
+    # plt.show()
+
+    print('TEST: sg env det')
+    #data = data + noise
+    #data[n_samples//2:] *= 0.5
+    butter_filter = SGBandEnvelopeDetector((9, 12), 250, 0.9, 151, 2)
+    #plt.plot(data)
+    plt.plot(np.hstack([butter_filter.apply(data[k * 8:(k + 1) * 8]) for k in range(n_samples // 8)]))
     plt.show()
