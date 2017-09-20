e69e17af71ce2c114f1bc4c7e212a3b863635f6e
nsmetanin <smtnm@ya.ru>
2017-09-20 19:08:54 +0300

    Bci mu bci metrics postprocessing

--- pynfb/postprocessing/bci_mu_bci_metrics_proc.py
+++ pynfb/postprocessing/bci_mu_bci_metrics_proc.py
@@ -2,16 +2,45 @@ import pandas as pd
 from scipy.stats import linregress
 import pylab as plt
 import seaborn as sns
+import numpy as np
 
-df = pd.read_csv('bcinfbbci_metrics.csv')
+from scipy import stats
 
-subj = 5
-day = 2
 
-avg_before = df[df['subj'] == subj][df['day'] == day][df['after'] == 0].mean()
-avg_after  = df[df['subj'] == subj][df['day'] == day][df['after'] == 0].mean()
-slope = avg_before['fb_slope']
-acc_train_diff = avg_before[['acc_train', 'acc_train0', 'acc_train1', 'acc_train2']]
-acc_test_diff = avg_before[['acc_test', 'acc_test0', 'acc_test1', 'acc_test2']]
-print(acc_train_diff)
-print(acc_test_diff)
+def get_acc_slopes(filename, n_subj):
+    df = pd.read_csv(filename)
+    slopes = []
+    acc_diff = []
+    for day in range(3):
+        for subj in range(n_subj):
+            avg_before = df[df['subj'] == subj][df['day'] == day][df['after'] == 0].mean()
+            avg_after  = df[df['subj'] == subj][df['day'] == day][df['after'] == 1].mean()
+            slopes.append(avg_before['fb_slope'] if avg_before['fb_slope']>-0.1 else 0)
+            acc_diff.append(avg_after['acc_train'] - avg_before['acc_train'])
+    return np.array(slopes), np.array(acc_diff)
+
+slopes, acc_diff = get_acc_slopes('bcinfbbci_metrics.csv', 4)
+slopes_mock, acc_diff_mock = get_acc_slopes('bcinfbbci_mock_metrics.csv', 4)
+
+f, axes = plt.subplots(1, 3)
+
+sns.regplot(slopes, acc_diff, ax=axes[0])
+sns.regplot(slopes_mock, acc_diff_mock, ax=axes[0])
+axes[0].set_title('p_real={:.3f}\np_mock={:.3f}'.format(stats.linregress(slopes, acc_diff).pvalue, stats.linregress(slopes_mock, acc_diff_mock).pvalue))
+axes[0].legend(['real', 'mock'])
+axes[0].set_xlim(-0.003, 0.003)
+axes[0].set_ylim(-0.15, 0.15)
+
+sns.boxplot(x=np.concatenate([['real']*len(slopes), ['mock']*len(slopes_mock)]), y=np.concatenate([slopes, slopes_mock]), ax=axes[1])
+axes[1].set_ylabel('slope')
+axes[1].set_title('p={:.3f}'.format(stats.ttest_ind(slopes, slopes_mock)[1]))
+axes[1].legend(['real', 'mock'])
+
+sns.boxplot(x=np.concatenate([['real']*len(acc_diff), ['mock']*len(acc_diff_mock)]), y=np.concatenate([acc_diff, acc_diff_mock]), ax=axes[2])
+axes[2].set_ylabel('acc_diff')
+axes[2].set_title('p={:.3f}'.format(stats.ttest_ind(acc_diff, acc_diff_mock)[1]))
+axes[2].legend(['real', 'mock'])
+
+plt.tight_layout()
+plt.show()
+
