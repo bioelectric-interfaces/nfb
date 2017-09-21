c098d1f2f1d6998e4228076ee91e9fd14b79dd16
nsmetanin <smtnm@ya.ru>
2017-09-21 20:07:20 +0300

    Added methods to implement

--- pynfb/widgets/signals_recorder.py
+++ pynfb/widgets/signals_recorder.py
@@ -7,7 +7,7 @@ paired_colors = ['#dbae57','#57db6c','#dbd657','#57db94','#b9db57','#57dbbb','#9
 
 
 class SignalPainter(pg.PlotWidget):
-    def __init__(self, fs, n_signals, seconds_to_plot=5, overlap=False, names=None, **kwargs):
+    def __init__(self, fs, names, seconds_to_plot=5, overlap=False, **kwargs):
         super(SignalPainter, self).__init__(**kwargs)
         # gui settings
         #self.getPlotItem().hideAxis('left')
@@ -16,17 +16,23 @@ class SignalPainter(pg.PlotWidget):
         self.getPlotItem().setMouseEnabled(x=False, y=False)
         self.setBackgroundBrush(pg.mkBrush('#252120'))
 
+        # init buffers
+        self.n_signals = len(names)
+        self.n_samples = int(fs * seconds_to_plot) # samples to show
+        self.previous_pos = 0 # resieved samples counter
+        self.x_mesh = np.linspace(0, seconds_to_plot, self.n_samples)
+        self.y_raw_buffer = np.zeros(shape=(self.n_samples, self.n_signals)) * np.nan
+
         # set names
-        if names is not None:
-            if not overlap:
-                self.getPlotItem().getAxis('left').setTicks(
-                    [[(val, tick) for val, tick in zip(range(1, n_channels + 1), names)]])
-            else:
-                self.getPlotItem().addLegend(offset=(-30, 30))
+        if not overlap:
+            self.getPlotItem().getAxis('left').setTicks(
+                [[(val, tick) for val, tick in zip(range(1, self.n_signals + 1), names)]])
+        else:
+            self.getPlotItem().addLegend(offset=(-30, 30))
 
         # init signal curves
         self.curves = []
-        for i in range(n_signals):
+        for i in range(self.n_signals):
             print(names[i] if names else '')
             curve = pg.PlotDataItem(pen=paired_colors[i%len(paired_colors)], name=names[i] if names else '')
             self.addItem(curve)
@@ -36,15 +42,10 @@ class SignalPainter(pg.PlotWidget):
 
         # add vertical running line
         self.vertical_line = pg.PlotDataItem(pen=pg.mkPen(color='B48375', width=1))
-        self.vertical_line_height = [0, n_signals + 1] if not overlap else [-1, 1]
+        self.vertical_line_height = [0, self.n_signals + 1] if not overlap else [-1, 1]
         self.addItem(self.vertical_line)
 
-        # init buffers
-        self.n_samples = fs * seconds_to_plot # samples to show
-        self.n_signals = n_signals
-        self.previous_pos = 0 # resieved samples counter
-        self.x_mesh = np.linspace(0, seconds_to_plot, self.n_samples)
-        self.y_raw_buffer = np.zeros(shape=(self.n_samples, n_signals)) * np.nan
+
 
     def update(self, chunk):
         chunk_len = len(chunk)
@@ -68,11 +69,20 @@ class SignalPainter(pg.PlotWidget):
     def modify(self, chunk):
         return chunk
 
+    def update_std(self, chunk):
+        pass
+
+    def set_chunk(self, chunk):
+        self.update(chunk)
+
+    def update_levels(self):
+        pass
+
 if __name__ == '__main__':
     fs = 250
     sec_to_plot = 10
     n_samples = sec_to_plot * fs
-    n_channels = 30
+    n_channels = 10
     chunk_len = 8
 
     data = np.random.normal(size=(100000, n_channels)) * 1
@@ -80,7 +90,7 @@ if __name__ == '__main__':
     data = signal.lfilter(b, a, data, axis=0)
 
     a = QtGui.QApplication([])
-    w = SignalPainter(fs, n_channels, names=['ch'+str(j) for j in range(n_channels)])
+    w = SignalPainter(fs, ['ch'+str(j) for j in range(n_channels)])
 
     time = 0
     def update():
