4ad3bc9b049ce5a5ff73dc85b1c3d1d95dd8ff23
kolai <n.m.smetanin@gmail.com>
2018-06-18 13:55:06 +0300

    Montage fix

--- pynfb/inlets/montage.py
+++ pynfb/inlets/montage.py
@@ -29,23 +29,26 @@ def azimuthal_equidistant_projection(hsp):
 class Montage(pd.DataFrame):
     CHANNEL_TYPES = ['EEG', 'MAG', 'GRAD', 'OTHER']
 
-    def __init__(self, names):
-        super(Montage, self).__init__(columns=['name', 'type', 'pos_x', 'pos_y'])
-        layout_eeg = Montage.load_layout('EEG1005')
-        layout_mag = Montage.load_layout('Vectorview-mag')
-        layout_grad = Montage.load_layout('Vectorview-grad')
-        for name in names:
-            if name.upper() in layout_eeg.names:
-                ch_ind = layout_eeg.names.index(name.upper())
-                self._add_channel(name, 'EEG', layout_eeg.pos[ch_ind][:2])
-            elif name.upper() in layout_mag.names:
-                ch_ind = layout_mag.names.index(name.upper())
-                self._add_channel(name, 'MAG', layout_mag.pos[ch_ind][:2])
-            elif name.upper() in layout_grad.names:
-                ch_ind = layout_grad.names.index(name.upper())
-                self._add_channel(name, 'GRAD', layout_grad.pos[ch_ind][:2])
-            else:
-                self._add_channel(name, 'OTHER', (None, None))
+    def __init__(self, names, **kwargs):
+        if not isinstance(names, list):
+            super(Montage, self).__init__(names, **kwargs)
+        else:
+            super(Montage, self).__init__(columns=['name', 'type', 'pos_x', 'pos_y'])
+            layout_eeg = Montage.load_layout('EEG1005')
+            layout_mag = Montage.load_layout('Vectorview-mag')
+            layout_grad = Montage.load_layout('Vectorview-grad')
+            for name in names:
+                if name.upper() in layout_eeg.names:
+                    ch_ind = layout_eeg.names.index(name.upper())
+                    self._add_channel(name, 'EEG', layout_eeg.pos[ch_ind][:2])
+                elif name.upper() in layout_mag.names:
+                    ch_ind = layout_mag.names.index(name.upper())
+                    self._add_channel(name, 'MAG', layout_mag.pos[ch_ind][:2])
+                elif name.upper() in layout_grad.names:
+                    ch_ind = layout_grad.names.index(name.upper())
+                    self._add_channel(name, 'GRAD', layout_grad.pos[ch_ind][:2])
+                else:
+                    self._add_channel(name, 'OTHER', (None, None))
 
     def _add_channel(self, name, type, pos):
         self.loc[len(self)] = {'name': name, 'type': type, 'pos_x': pos[0], 'pos_y': pos[1]}
