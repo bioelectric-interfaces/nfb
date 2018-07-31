f79162f6eeb6b9c2caf2257075afcfad87316a92
nikolaims <n.m.smetanin@gmail.com>
2018-07-31 16:01:34 +0300

    Run experiment by script directly from xml design file

---
+++ pynfb/run_design.py
@@ -0,0 +1,33 @@
+import sys, os, argparse, importlib
+from PyQt4 import QtGui
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--nfblab')
+parser.add_argument('--design')
+args = parser.parse_args()
+
+# validate arguments
+if args.design is None or args.nfblab is None:
+    raise SyntaxError('\nPlease provide all arguments: '
+                      '\n\t --nfblab Path to nfb directory'
+                      '\n\t --design Path to design file')
+
+if not os.path.isfile(args.design):
+    raise FileNotFoundError('No such design file {}. Please, correct --design value.'.format(args.design))
+
+if not os.path.isdir(args.nfblab):
+    raise FileNotFoundError("Path to nfblab doesn't exist. Please, correct --nfblab value.")
+
+sys.path.insert(0, args.nfblab)
+if importlib.util.find_spec('pynfb') is None:
+    raise ImportError('Cannot import nfblab package. Please, correct --nfblab value')
+
+# import NFBLab
+from pynfb.experiment import Experiment
+from pynfb.io.xml_ import xml_file_to_params
+
+# run experiment
+app = QtGui.QApplication(sys.argv)
+experiment = Experiment(app, xml_file_to_params(args.design))
+sys.exit(app.exec_())
