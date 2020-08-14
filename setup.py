9e0b8500850c9c7ca05ceb20f7d92d7717dc2ad7
andreasxp <andres.zhukov@gmail.com>
2020-08-14 17:52:35 +0300

    Add setup script and .gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Setup script for nfb package.
+Warning: nfb requires you install some outdated versions of packages. Consider installing nfb in a virtual enviroment,
+using tools such as venv or conda.
+"""
+from setuptools import setup
+
+install_requires = [
+    "h5py",
+    "pyqt5",
+    "pyopengl",
+    "pyqtgraph",
+    "seaborn",
+    "scikit-learn",
+    "sympy",
+    "mne==0.12",
+    "pylsl"
+]
+
+setup(
+    name="nfb",
+    description="Conduct experiments in real-time EEG/MEG paradigm",
+    install_requires=install_requires
+)
