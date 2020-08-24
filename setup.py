d2c8ca9d66658c3fe89d2c8e39a0d6a3d5fe0fff
andreasxp <andres.zhukov@gmail.com>
2020-08-24 12:31:10 +0300

    Rename module, fix extras_require

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-"""Setup script for nfb package.
-Warning: nfb requires you install some outdated versions of packages. Consider installing nfb in a virtual enviroment,
-using tools such as venv or conda.
+"""Setup script for pynfb package.
+Warning: pynfb requires you install some outdated versions of packages. Consider installing pynfb in a virtual
+enviroment, using tools such as venv or conda.
 """
 from setuptools import setup
 
@@ -22,8 +22,9 @@ extras_require = {
 }
 
 setup(
-    name="nfb",
+    name="pynfb",
     version="0.1",
     description="Conduct experiments in real-time EEG/MEG paradigm",
-    install_requires=install_requires
+    install_requires=install_requires,
+    extras_require=extras_require
 )
