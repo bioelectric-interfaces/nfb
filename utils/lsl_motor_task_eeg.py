3274c314d8e4d29b5e9475f39e3bdeb36d41177c
nikolaims <n.m.smetanin@gmail.com>
2020-11-12 19:40:44 +0300

    lsl stream motor task gui

--- utils/lsl_motor_task_eeg.py
+++ utils/lsl_motor_task_eeg.py
@@ -1,6 +1,5 @@
 import numpy as np
 import time
-from threading import Thread
 import os
 from pylsl import StreamInfo, StreamOutlet
 # stream info
@@ -63,13 +62,13 @@ class LSLSim:
             # print(len(data), self.current_block)
         self.stream.push_chunk(chunk.tolist())
         self.total_samples += self.chunk_size
-        # push chunk
         # print('push chunk {}x{}, current sample {}'.format(*chunk.shape, self.current_sample))
 
     def set_state_by_name(self, state_name):
         self.current_state = self.states_names.index(state_name)
         self.current_sample = 0
         self.state_block_numbers = self._get_block_numbers()
+        self.current_block = self._pick_random_block()
         self.next_block = self._pick_random_block()
         print('{} Current state changed to {} {}'.format(self.time_str(), self.current_state, state_name))
 
@@ -95,6 +94,8 @@ class LSLSim:
 
 
 if __name__ == '__main__':
+    GUI = True
+
     try:
         from google_drive_downloader import GoogleDriveDownloader as gdd
     except ModuleNotFoundError as e:
@@ -117,36 +118,78 @@ if __name__ == '__main__':
 
     lsl_sim = LSLSim(stream_name, fs, df, chunk_size)
 
-    break_flag = False
-    def run_lsl_sim(n_seconds=10):
-        print('{} Start streaming'.format(lsl_sim.time_str()))
-        for k in range(n_seconds*fs//chunk_size):
-            lsl_sim.push_chunk()
-            time.sleep(chunk_size/fs)
-            if break_flag: break
-        del lsl_sim.stream
-        print(lsl_sim.info())
-        print('{} Stop streaming'.format(lsl_sim.time_str()))
-        print('... Press any key to exit ...')
+    # command line interface
 
-    thread = Thread(target=run_lsl_sim, daemon=True)
-    thread.start()
-
-    print(lsl_sim.info())
-    while thread.is_alive():
-        command = input('{} Enter state name or command "/stop", "/info":\n'.format(lsl_sim.time_str()))
-        if not thread.is_alive(): break
-        if command in lsl_sim.states_names:
-            lsl_sim.state = command
-        elif command == '/stop':
-            break_flag = True
-            thread.join()
-            input()
-        elif command == '/info':
+    if not GUI:
+        break_flag = False
+        def run_lsl_sim(n_seconds=10):
+            print('{} Start streaming'.format(lsl_sim.time_str()))
+            for k in range(n_seconds * fs // chunk_size):
+                lsl_sim.push_chunk()
+                time.sleep(chunk_size / fs)
+                if break_flag: break
+            del lsl_sim.stream
             print(lsl_sim.info())
-        else:
-            print("{} State name or command \"{}\" doesn't exist".format(lsl_sim.time_str(), command))
-        time.sleep(0.5)
+            print('{} Stop streaming'.format(lsl_sim.time_str()))
+            print('... Press any key to exit ...')
+        from threading import Thread
+        thread = Thread(target=run_lsl_sim, daemon=True)
+        thread.start()
+        print(lsl_sim.info())
+        while thread.is_alive():
+            command = input('{} Enter state name or command "/stop", "/info":\n'.format(lsl_sim.time_str()))
+            if not thread.is_alive(): break
+            if command in lsl_sim.states_names:
+                lsl_sim.state = command
+            elif command == '/stop':
+                break_flag = True
+                thread.join()
+                input()
+            elif command == '/info':
+                print(lsl_sim.info())
+            else:
+                print("{} State name or command \"{}\" doesn't exist".format(lsl_sim.time_str(), command))
+            time.sleep(0.5)
+        if not break_flag:
+            thread.join()
 
-    if not break_flag:
-        thread.join()
+    else:
+        from PyQt5.QtWidgets import *
+        from PyQt5.QtCore import QTimer
+        app = QApplication([])
+        window = QWidget()
+        states_widget = QWidget()
+        main_layout = QVBoxLayout()
+        layout = QHBoxLayout()
+        button = QPushButton('Rest')
+        button.pressed.connect(lambda: lsl_sim.set_state_by_name('Rest'))
+        layout.addWidget(button)
+        button = QPushButton('Left')
+        button.pressed.connect(lambda: lsl_sim.set_state_by_name('Left'))
+        layout.addWidget(button)
+        button = QPushButton('Right')
+        button.pressed.connect(lambda: lsl_sim.set_state_by_name('Right'))
+        layout.addWidget(button)
+        button = QPushButton('Legs')
+        button.pressed.connect(lambda: lsl_sim.set_state_by_name('Legs'))
+        layout.addWidget(button)
+        states_widget.setLayout(layout)
+        window.setLayout(main_layout)
+        main_layout.addWidget(states_widget)
+        text = QLabel('Wow')
+        main_layout.addWidget(text)
+        def step():
+            lsl_sim.push_chunk()
+            text.setText(lsl_sim.info())
+        window.show()
+        timer = QTimer()
+        timer.timeout.connect(step)
+        timer.start(chunk_size/fs*1000)
+        print('{} Start streaming'.format(lsl_sim.time_str()))
+        def on_close():
+            del lsl_sim.stream
+            print(lsl_sim.info())
+            print('{} Stop streaming'.format(lsl_sim.time_str()))
+        app.aboutToQuit.connect(on_close)
+        app.exec_()
+
