3e2bd0ae9e681435b86243ed9045a520e97725fc
nikolaims <n.m.smetanin@gmail.com>
2020-12-28 21:04:22 +0300

    FB band fixes

--- utils/audiofb/subj_stim.py
+++ utils/audiofb/subj_stim.py
@@ -3,9 +3,11 @@ from pynfb.inlets.lsl_inlet import LSLInlet
 from time import sleep
 from psychopy import visual, core
 import numpy as np
-from pynfb.signal_processing.filters import CFIRBandEnvelopeDetector, DownsampleFilter, SpatialFilter, IdentityFilter
+from pynfb.signal_processing.filters import CFIRBandEnvelopeDetector, DownsampleFilter, SpatialFilter, IdentityFilter, ExponentialSmoother
 from pynfb.outlets.signals_outlet import SignalsOutlet
 from utils.audiofb.volume_controller import VolumeController
+
+FS_OUT = 250
 
 # init psychopy window
 win = visual.Window([600, 600])
@@ -26,11 +28,11 @@ volume_controller.set_volume(0)
 message.text = 'Сообщение экспериментатору:\nПодключение к LSL потоку "NVX136_Data"...'
 win.flip()
 lsl_in = LSLInlet('NVX136_Data')
-fs = int(lsl_in.get_frequency())
+fs_in = int(lsl_in.get_frequency())
 channels = lsl_in.get_channels_labels()
 
 # create LSL outlet
-lsl_out = SignalsOutlet(channels, fs=500, name='NVX136_FB')
+lsl_out = SignalsOutlet(channels, fs=FS_OUT, name='NVX136_FB')
 
 # connect to NFBLab
 message.text = 'Сообщение экспериментатору:\nЗапустите эксперимент в NFBLab'
@@ -38,15 +40,15 @@ win.flip()
 server = SimpleServer()
 
 # setup filters
-downsampler = DownsampleFilter(10, len(channels))
+downsampler = DownsampleFilter(int(fs_in / FS_OUT), len(channels))
 spatial_filter = np.zeros(len(channels))
-cfir = CFIRBandEnvelopeDetector([8, 12], fs, IdentityFilter())
+cfir = CFIRBandEnvelopeDetector([8, 12], FS_OUT, ExponentialSmoother(0.), n_taps=500)
 mean = 0
 std = 1
 
 # init values
 score = np.zeros(1)
-
+play_feedback = False
 while 1:
     # receive and process chunk
     chunk, timestamp = lsl_in.get_next_chunk()
@@ -58,7 +60,12 @@ while 1:
         if len(chunk) > 0:
             virtual_channel = chunk.dot(spatial_filter)
             envelope = cfir.apply(virtual_channel)
-            score = (envelope - mean)/std if std > 0 else 404
+            score = (envelope - mean)/(std if std > 0 else 1)
+            print(envelope, score)
+
+            if play_feedback:
+                volume = (np.tanh(score[-1]) / 2 + 0.5) * 100
+                volume_controller.set_volume(volume)
 
             # push down-sampled chunk to lsl outlet
             lsl_out.push_chunk(chunk.tolist())
@@ -68,13 +75,13 @@ while 1:
     if meta_str is None:
         continue
     elif meta_str == 'msg':  # baseline blocks (set message and stop NFB)
+        play_feedback = False
         volume_controller.set_volume(0)
         print('Dummy.. Set message to "{}"'.format(obj))
         message.text = obj
         win.flip()
     elif meta_str == 'fb1':  # fb blocks
-        volume = (np.tanh(score[-1])/2+0.5)*30+70
-        volume_controller.set_volume(volume)
+        play_feedback = True
         print('Dummy.. Run FB. Set message to "{}"'.format(obj))
         message.text = obj
         win.flip()
@@ -82,7 +89,7 @@ while 1:
         spatial_filter = obj
         print('Dummy.. Set spatial filter to {}'.format(obj))
     elif meta_str == 'bnd':  # update bandpass filter
-        cfir = CFIRBandEnvelopeDetector(obj, fs, IdentityFilter())
+        cfir = CFIRBandEnvelopeDetector(obj, FS_OUT, ExponentialSmoother(0.), n_taps=500)
         print('Dummy.. Set band to {}'.format(obj))
     elif meta_str == 'std':  # update fb score stats
         mean, std = obj
