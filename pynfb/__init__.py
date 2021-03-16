1a4f85b388da365fea3418712590275cf13928f6
andreasxp <andres.zhukov@gmail.com>
2021-03-16 16:06:01 +0300

    Bump version to 1.0.0

--- pynfb/__init__.py
+++ pynfb/__init__.py
@@ -20,6 +20,8 @@ import os
 from importlib import import_module
 STATIC_PATH = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/static')
 
+__version__ = "1.0.0"
+
 # pylsl has a dll order problem with kiwisolver (which is used by matplotlib). If kiwisolver is first imported before
 # pylsl, pylsl will not find any streams. Import this package (pynfb) in your entry point to work around the problem.
 import_module("pylsl")
