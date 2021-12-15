ab59a53334167358f089f2b1e4943209d06e174a
chrisjturner <chrisjturner@gmail.com>
2021-12-15 13:43:41 +0000

    Enable lsl stream discovery

--- pynfb/settings_widget/inlet.py
+++ pynfb/settings_widget/inlet.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui, QtWidgets
+import pylsl
 
 inlet_types = ['lsl', 'lsl_from_file', 'lsl_generator', 'ftbuffer']
 
@@ -12,6 +13,10 @@ class InletSettingsWidget(QtWidgets.QWidget):
         self.combo.addItem('LSL from file')
         self.combo.addItem('LSL generator')
         self.combo.addItem('Field Trip buffer')
+        self.lsl_button = QtWidgets.QPushButton()
+        self.lsl_button.setText("find LSL streams")
+        self.lsl_button.setEnabled(False)
+        self.lsl_button.clicked.connect(self.find_lsl_streams)
         self.line_edit_1 = QtWidgets.QLineEdit()
         self.line_edit_1.textChanged.connect(self.line_edit_1_changed_event)
         self.line_edit_2 = QtWidgets.QLineEdit()
@@ -22,6 +27,9 @@ class InletSettingsWidget(QtWidgets.QWidget):
         # self.raw_path.textChanged.connect(self.raw_path_changed_event)
         self.raw_select_button = QtWidgets.QPushButton('Select file...')
         self.raw_select_button.clicked.connect(self.chose_file_action)
+        self.lsl_stream_select_comboBox = QtWidgets.QComboBox()
+        self.lsl_stream_select_comboBox.setEnabled(False)
+        self.lsl_stream_select_comboBox.activated.connect(self.set_lsl_stream)
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.combo)
         layout.addWidget(self.line_edit_1)
@@ -30,7 +38,11 @@ class InletSettingsWidget(QtWidgets.QWidget):
         # layout.addWidget(self.raw_path)
         layout.addWidget(self.raw_select_button)
         layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(layout)
+        layout_v = QtWidgets.QVBoxLayout()
+        layout_v.addLayout(layout)
+        layout_v.addWidget(self.lsl_button)
+        layout_v.addWidget(self.lsl_stream_select_comboBox)
+        self.setLayout(layout_v)
         self.combo.currentIndexChanged.connect(self.combo_changed_event)
         self.combo.setCurrentIndex(inlet_types.index(self.parent().params['sInletType']))
         self.combo_changed_event()
@@ -58,14 +70,20 @@ class InletSettingsWidget(QtWidgets.QWidget):
         if self.combo.currentIndex() == 0:
             self.line_edit_1.setPlaceholderText('Print LSL stream name')
             self.line_edit_1.setText(self.parent().params['sStreamName'])
+            self.lsl_stream_select_comboBox.setEnabled(True)
+            self.lsl_button.setEnabled(True)
         elif self.combo.currentIndex() == 1:
             self.raw_select_button.show()
             self.line_edit_1.setPlaceholderText('Print raw data file path')
             self.line_edit_1.setText(self.parent().params['sRawDataFilePath'])
+            self.lsl_stream_select_comboBox.setEnabled(False)
+            self.lsl_button.setEnabled(False)
         elif self.combo.currentIndex() == 2:
             self.line_edit_1.setPlaceholderText('')
             self.line_edit_1.setEnabled(False)
             self.line_edit_1.setText('')
+            self.lsl_stream_select_comboBox.setEnabled(False)
+            self.lsl_button.setEnabled(False)
         elif self.combo.currentIndex() == 3:
             host, port = self.parent().params['sFTHostnamePort'].split(':')
             self.line_edit_2.show()
@@ -73,6 +91,17 @@ class InletSettingsWidget(QtWidgets.QWidget):
             self.line_edit_2.setPlaceholderText('Port')
             self.line_edit_1.setText(host)
             self.line_edit_2.setText(port)
+            self.lsl_stream_select_comboBox.setEnabled(False)
+            self.lsl_button.setEnabled(False)
+
+    def find_lsl_streams(self):
+        self.streams = {x.name(): x.source_id() for x in pylsl.resolve_streams()}
+        # populate the lsl streams combo box with available streams
+        self.lsl_stream_select_comboBox.addItems(list(self.streams.keys()))
+
+    def set_lsl_stream(self):
+        self.line_edit_1.setText(self.lsl_stream_select_comboBox.currentText())
+
 
     def chose_file_action(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', './')[0]
