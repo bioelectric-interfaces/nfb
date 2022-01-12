06e7c06c4b621548ac143e7bd1182ee580ec586e
chrisjturner <chrisjturner@gmail.com>
2022-01-12 23:16:45 +0000

    filter EOG data for whole dataset

--- analysis/pilot_analysis_202201.py
+++ analysis/pilot_analysis_202201.py
@@ -32,12 +32,19 @@ h5file = "/Users/christopherturner/Documents/EEG Data/ChrisPilot20220110/0-pre_t
 df1, fs, channels, p_names = load_data(h5file)
 df1['sample'] = df1.index
 
+# Low pass filter (20Hz) the EOG/ECG channels
+cutoff = 20
+df1['ECG_FILTERED'] = butter_lowpass_filter(df1['ECG'], cutoff, fs)
+df1['EOG_FILTERED'] = butter_lowpass_filter(df1['EOG'], cutoff, fs)
+
 # Extract the individual protocols
 protocol_data = {}
 block_numbers = df1['block_number'].unique()
 protocol_names = [f"{a_}{b_}"for a_, b_ in zip(p_names, block_numbers)]
 channels_signal = channels.copy()
 channels_signal.append("signal_AAI")
+channels_signal.append("EOG_FILTERED")
+channels_signal.append("ECG_FILTERED")
 df2 = pd.melt(df1, id_vars=['block_name', 'block_number', 'sample', 'choice', 'probe', 'answer'], value_vars=channels_signal, var_name="channel", value_name='data')
 
 for protocol_n in block_numbers:
@@ -45,15 +52,15 @@ for protocol_n in block_numbers:
 
 
 # --- Pre/Post tests----
-# - Prepare EOG and ECG signals
 
-# get raw eog data
-eog_data = protocol_data['EyeCalib2'].loc[protocol_data['EyeCalib2']['channel'].isin(["ECG", "EOG"])]
+# get filtered eog data
+eog_data = protocol_data['EyeCalib2'].loc[protocol_data['EyeCalib2']['channel'].isin(["ECG_FILTERED", "EOG_FILTERED"])]
 
+# - CALIBRATION -
 # get samples for onset of each calibration stage (probe: left=10, right=11, top=12, bottom=13, cross=14)
 eog_data['probe_change'] = eog_data['probe'].diff()
 calibration_samples = eog_data[eog_data['probe_change'] != 0]
-calibration_samples = calibration_samples.loc[protocol_data['EyeCalib2']['channel'].isin(["EOG"])][['sample', 'probe']]
+calibration_samples = calibration_samples.loc[protocol_data['EyeCalib2']['channel'].isin(["EOG_FILTERED"])][['sample', 'probe']]
 calibration_samples['probe'] = calibration_samples['probe'].replace({14: 'cross', 10: 'left', 11: 'right', 12: 'top', 13: 'bottom'})
 
 # fig = px.line(eog_data, x="sample", y="data", color='channel')
@@ -62,10 +69,6 @@ calibration_samples['probe'] = calibration_samples['probe'].replace({14: 'cross'
 #                   annotation_text=row['probe'],
 #                   annotation_position="bottom right")
 # fig.show()
-
-# Low pass filter (20Hz)
-cutoff = 20
-eog_data['filtered_data'] = butter_lowpass_filter(eog_data['data'], cutoff, fs)
 
 # Get the offsets for each calibration point
 calibration_delay = 400 # 500ms to react to probe # TODO: find a way to automate this
@@ -80,18 +83,21 @@ for idx in range(len(calibration_samples)):
         previous_type = type
     else:
         second_EOG = eog_data[eog_data['sample'].between(previous_offset + calibration_delay, offset + calibration_delay, inclusive="neither")]
-        calibration_offsets_ecg[previous_type] = second_EOG.loc[second_EOG['channel'] == "EOG"]['filtered_data'].median()
-        calibration_offsets_eog[previous_type] = second_EOG.loc[second_EOG['channel'] == "ECG"]['filtered_data'].median()
+        calibration_offsets_ecg[previous_type] = second_EOG.loc[second_EOG['channel'] == "EOG_FILTERED"]['data'].median()
+        calibration_offsets_eog[previous_type] = second_EOG.loc[second_EOG['channel'] == "ECG_FILTERED"]['data'].median()
         previous_offset = offset
         previous_type = type
 
     if idx == len(calibration_samples)-1:
         type = 'cross2'
         second_EOG = eog_data[eog_data['sample'].between(offset + calibration_delay, eog_data['sample'].iloc[-1], inclusive="neither")]
-        calibration_offsets_ecg[type] = second_EOG.loc[second_EOG['channel'] == "EOG"]['filtered_data'].median()
-        calibration_offsets_eog[type] = second_EOG.loc[second_EOG['channel'] == "ECG"]['filtered_data'].median()
+        calibration_offsets_ecg[type] = second_EOG.loc[second_EOG['channel'] == "EOG_FILTERED"]['data'].median()
+        calibration_offsets_eog[type] = second_EOG.loc[second_EOG['channel'] == "ECG_FILTERED"]['data'].median()
 
-fig = px.line(eog_data, x="sample", y=eog_data["filtered_data"], color='channel')
+# TODO: do the above but with MNE (to try get automatic eog events)
+
+
+fig = px.line(eog_data, x="sample", y=eog_data["data"], color='channel')
 for index, row in calibration_samples.iterrows():
     fig.add_vline(x=row['sample']+calibration_delay, line_dash="dot",
                   annotation_text=row['probe'],
@@ -100,18 +106,21 @@ for type, value in calibration_offsets_eog.items():
     fig.add_hline(y=value, line_color='red',
                       annotation_text=f"{type}: MEDIAN",
                       annotation_position="bottom right")
-for type, value in calibration_offsets_ecg.items():
-    fig.add_hline(y=value, line_color='blue',
-                      annotation_text=f"{type}: MEDIAN",
-                      annotation_position="bottom right")
+# for type, value in calibration_offsets_ecg.items():
+#     fig.add_hline(y=value, line_color='blue',
+#                       annotation_text=f"{type}: MEDIAN",
+#                       annotation_position="bottom right")
 fig.show()
 
 pass
 
-# Plot calibration location on spatial grid
-# Plot eye tracking data on spatial grid definied by calibration
-
 # Calculate fixation bias for each trial
+for protocol, data in protocol_data.items():
+    if "fix_cross" in protocol:
+        pass
+    elif "image" in protocol:
+        pass
+
 # Average fixation bias over all trials
 # Get change in fixation bias between pre and post sessions
 # Do permutation test for individual data
