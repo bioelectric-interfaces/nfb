3ccc19940191150f33ba06f1b318ddf328e8e21b
Christopher John Turner (PGR) <2354158T@student.gla.ac.uk>
2022-01-18 13:29:10 +0000

    Update erp_analysis.py

--- analysis/erp_analysis.py
+++ analysis/erp_analysis.py
@@ -27,7 +27,7 @@ import mne
 mne.viz.set_3d_backend('pyvista')
 
 # ------ Get data files
-data_directory = "/Users/christopherturner/Documents/EEG_Data/pilot_202201" # This is the directory where all participants are in
+data_directory = "/Users/2354158T/Documents/EEG_Data/pilot_202201" # This is the directory where all participants are in
 
 # get participants
 participants = next(os.walk(data_directory))[1]
@@ -91,6 +91,7 @@ for participant, participant_dirs in experiment_dirs.items():
                         columns=['signal_Alpha_Left', 'signal_Alpha_Right', 'signal_AAI', 'events', 'reward', 'choice', 'answer', 'probe', 'block_name',
                                  'block_number', 'sample', 'MKIDX', 'protocol_change', 'choice_events'])
 
+
                     # create an MNE info
                     m_info = mne.create_info(ch_names=list(eeg_data.columns), sfreq=fs, ch_types=['eeg' for ch in list(eeg_data.columns)])
 
@@ -122,75 +123,79 @@ for participant, participant_dirs in experiment_dirs.items():
 
                     # # low pass at 40hz
                     m_filt = m_raw.copy()
-                    m_filt.filter(l_freq=8, h_freq=12)
+                    m_filt.filter(l_freq=0, h_freq=40)
 
                     # # epoch the data
                     events = mne.find_events(m_raw, stim_channel='STI')
+                    picks = [c for c in m_info.ch_names if c not in ['EOG', 'ECG']]
                     epochs = mne.Epochs(m_filt, events, event_id=event_dict, tmin=-0.2, tmax=0.5,
-                                        preload=True)
-                    fig = epochs.plot(events=events, scalings={"eeg":10})
+                                        preload=True, picks=picks, detrend=1)
+                    # fig = epochs.plot(events=events, scalings={"eeg":10})
 
                     # average epochs
                     choice_transition = epochs['choice_transition'].average()
                     fig2 = choice_transition.plot(spatial_colors=True)
                     # choice_transition.plot_joint()
 
+                    # plot topomap
+                    choice_transition.plot_topomap(times=[-0.2, 0.1, 0.4], average=0.05)
 
+                    pass
                     # ---------- SOURCE RECONSTRUCTION
                     # TODO: do this for epoched data (once get the above working)
                     # - first do this over the transition to choice
                     # TODO: make sure this is done the same way as the GUI (or compare the two if this one works)
                     # do initial calcs
-                    info = m_filt.info
-                    noise_cov = mne.compute_raw_covariance(m_filt, tmax=0.5) # LOOKS LIKE THIS NEEDS TO BE JUST RAW DATA - i.e. WITH NO EVENTS (OTHERWISE NEED TO DO THE EPOCH ONE AND FIND EVENTS) - PROBABLY GET THIS FROM BASELINE
-                    loose = 0.2
-                    depth = 0.8
-                    label = None # NOTE!!! LABELS ARE ONLY SUPPORTED WHEN DOING THIS IN SURFACE MODE
-
-                    # Get the forward solution for the specified source localisation type
-                    fs_dir = fetch_fsaverage(verbose=True)
-                    # --I think this 'trans' is like the COORDS2TRANSFORMATIONMATRIX
-                    trans = 'fsaverage'  # MNE has a built-in fsaverage transformation
-                    src = os.path.join(fs_dir, 'bem', 'fsaverage-ico-5-src.fif')
-                    bem = os.path.join(fs_dir, 'bem', 'fsaverage-5120-5120-5120-bem-sol.fif')
-                    fwd = mne.make_forward_solution(info, trans=trans, src=src,
-                                                    bem=bem, eeg=True, mindist=5.0, n_jobs=1)
-
-                    # label_name = surface_labels # TODO: check this - why do i get signal out with the bci test regardless of the label (though the amplitudes do change a little)
-                    # label = sd.get_labels(label_name)[0]
-
-                    inv = make_inverse_operator(info, fwd, noise_cov, loose=loose, depth=depth)
-                    # TODO: do this for epochs
-
-                    snr = 1.0  # use smaller SNR for raw data
-                    lambda2 = 1.0 / snr ** 2
-                    method = "sLORETA"  # use sLORETA method (could also be MNE or dSPM)
-                    start, stop = m_raw.time_as_index([0, 2])
-                    stc = apply_inverse_raw(m_filt, inv, lambda2, method, label,
-                                            start, stop, pick_ori=None)
-
-                    # Save result in stc files
-                    # stc.save('mne_%s_raw_inverse_%s' % (method, label_name))
-
-
-                    vertno_max, time_max = stc.get_peak(hemi=None)
-
-                    surfer_kwargs = dict(
-                        hemi='both',
-                        clim='auto', views='lateral',  # clim=dict(kind='value', lims=[8, 12, 15])
-                        initial_time=time_max, time_unit='s', size=(800, 800), smoothing_steps=10,
-                        surface='Pial', transparent=True, alpha=0.9, colorbar=True)
-                    brain = stc.plot(**surfer_kwargs)
-                    brain.add_foci(vertno_max, coords_as_verts=True, hemi='rh', color='blue',
-                                   scale_factor=0.6, alpha=0.5)
-                    brain.add_text(0.1, 0.9, 'dSPM (plus location of maximal activation)', 'title',
-                                   font_size=14)
-
-                    # Do a matplotlib to hold the brain plot in place!!
-                    plt.plot(1e3 * stc.times, stc.data[::100, :].T)
-                    plt.xlabel('time (ms)')
-                    plt.ylabel('%s value' % method)
-                    plt.show()
+                    # info = m_filt.info
+                    # noise_cov = mne.compute_raw_covariance(m_filt, tmax=0.5) # LOOKS LIKE THIS NEEDS TO BE JUST RAW DATA - i.e. WITH NO EVENTS (OTHERWISE NEED TO DO THE EPOCH ONE AND FIND EVENTS) - PROBABLY GET THIS FROM BASELINE
+                    # loose = 0.2
+                    # depth = 0.8
+                    # label = None # NOTE!!! LABELS ARE ONLY SUPPORTED WHEN DOING THIS IN SURFACE MODE
+                    #
+                    # # Get the forward solution for the specified source localisation type
+                    # fs_dir = fetch_fsaverage(verbose=True)
+                    # # --I think this 'trans' is like the COORDS2TRANSFORMATIONMATRIX
+                    # trans = 'fsaverage'  # MNE has a built-in fsaverage transformation
+                    # src = os.path.join(fs_dir, 'bem', 'fsaverage-ico-5-src.fif')
+                    # bem = os.path.join(fs_dir, 'bem', 'fsaverage-5120-5120-5120-bem-sol.fif')
+                    # fwd = mne.make_forward_solution(info, trans=trans, src=src,
+                    #                                 bem=bem, eeg=True, mindist=5.0, n_jobs=1)
+                    #
+                    # # label_name = surface_labels # TODO: check this - why do i get signal out with the bci test regardless of the label (though the amplitudes do change a little)
+                    # # label = sd.get_labels(label_name)[0]
+                    #
+                    # inv = make_inverse_operator(info, fwd, noise_cov, loose=loose, depth=depth)
+                    # # TODO: do this for epochs
+                    #
+                    # snr = 1.0  # use smaller SNR for raw data
+                    # lambda2 = 1.0 / snr ** 2
+                    # method = "sLORETA"  # use sLORETA method (could also be MNE or dSPM)
+                    # start, stop = m_raw.time_as_index([0, 2])
+                    # stc = apply_inverse_raw(m_filt, inv, lambda2, method, label,
+                    #                         start, stop, pick_ori=None)
+                    #
+                    # # Save result in stc files
+                    # # stc.save('mne_%s_raw_inverse_%s' % (method, label_name))
+                    #
+                    #
+                    # vertno_max, time_max = stc.get_peak(hemi=None)
+                    #
+                    # surfer_kwargs = dict(
+                    #     hemi='both',
+                    #     clim='auto', views='lateral',  # clim=dict(kind='value', lims=[8, 12, 15])
+                    #     initial_time=time_max, time_unit='s', size=(800, 800), smoothing_steps=10,
+                    #     surface='Pial', transparent=True, alpha=0.9, colorbar=True)
+                    # brain = stc.plot(**surfer_kwargs)
+                    # brain.add_foci(vertno_max, coords_as_verts=True, hemi='rh', color='blue',
+                    #                scale_factor=0.6, alpha=0.5)
+                    # brain.add_text(0.1, 0.9, 'dSPM (plus location of maximal activation)', 'title',
+                    #                font_size=14)
+                    #
+                    # # Do a matplotlib to hold the brain plot in place!!
+                    # plt.plot(1e3 * stc.times, stc.data[::100, :].T)
+                    # plt.xlabel('time (ms)')
+                    # plt.ylabel('%s value' % method)
+                    # plt.show()
                     pass
 
 # Get protocol with ERP
