2effbd7b5518408cf92f9b065731e6f222ed7277
chrisjturner <chrisjturner@gmail.com>
2022-02-08 00:54:34 +0000

    Update post_hoc_source.py

--- analysis/post_hoc_source.py
+++ analysis/post_hoc_source.py
@@ -21,6 +21,8 @@ from mne.datasets import eegbci
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import fetch_fsaverage
 from utils.load_results import load_data
+from mne.preprocessing import (ICA, create_eog_epochs, create_ecg_epochs,
+                               corrmap)
 
 mne.viz.set_3d_backend('pyvista')
 
@@ -39,14 +41,15 @@ df1['sample'] = df1.index
 
 # Drop non eeg data
 drop_cols = [x for x in df1.columns if x not in channels]
-drop_cols.extend(['MKIDX', 'EOG', 'ECG'])
+drop_cols.extend(['MKIDX'])
 eeg_data = df1.drop(columns=drop_cols)
 
 # Rescale the data (units are microvolts - i.e. x10^-6
 eeg_data = eeg_data * 1e-6
 
-# create an MNE info
-m_info = mne.create_info(ch_names=list(eeg_data.columns), sfreq=fs, ch_types=['eeg' for ch in list(eeg_data.columns)])
+# create an MNE info - set types appropriately
+m_info = mne.create_info(ch_names=list(eeg_data.columns), sfreq=fs, ch_types=['eeg' if ch not in ['ECG', 'EOG'] else 'eog' for ch in list(eeg_data.columns)])
+
 # Set the montage (THIS IS FROM roi_spatial_filter.py)
 standard_montage = mne.channels.make_standard_montage(kind='standard_1020')
 standard_montage_names = [name.upper() for name in standard_montage.ch_names]
@@ -67,20 +70,25 @@ m_raw.set_eeg_reference(projection=True)
 #########################
 # PRE-PROCESSING
 #########################
+# TODO: make this a function (or set of functions) that can be reused across scripts
+# TODO: have the option to save off the pre-processed data (for easier processing / reprocessing at later stages) i.e. so selecting bad channels doesn't need to happen every time
 """
 Gets rid of artefacts (various different sources of noise)
 Can be done automatically but should really be done visually to ensure good results
 """
 
-#-----------------------
-# TEMPORAL FILTERING
-#-----------------------
-"""
-Get a specific frequency band and remove non-relavant frequencies
-"""
-#non-causal, Infinite Impulse Response (IIR) Butterworth filter of 2nd order
-m_resting = m_raw.filter(l_freq=1, h_freq=40, iir_params={"order": 2}, method="iir")
-m_alpha = m_raw.filter(l_freq=8, h_freq=12, iir_params={"order": 2}, method="iir")
+# remove projectors
+ssp_projectors = m_raw.info['projs']
+m_raw.del_proj()
+
+# Look at psd
+m_raw.plot_psd(tmax=np.inf, fmax=250, average=True)
+m_raw.plot_psd(tmax=np.inf, fmax=250, spatial_colors=True)
+
+# Look at eye artefacts
+eog_epochs = mne.preprocessing.create_eog_epochs(m_raw, baseline=(-0.5, -0.2))
+eog_epochs.plot_image(combine='mean')
+eog_epochs.average().plot_joint()
 
 #-----------------------
 # BAD ELECTRODE DETECTION AND INTERPOLATION
@@ -88,6 +96,42 @@ m_alpha = m_raw.filter(l_freq=8, h_freq=12, iir_params={"order": 2}, method="iir
 """
 Remove electrodes whose values go above a certain range (from the resting filtered data)
 mne tutorial: https://mne.tools/stable/auto_tutorials/preprocessing/15_handling_bad_channels.html
+
+* Do this as early as possible with the raw object - bad channels propegate through the whole analysis
+
+* Detect bad channels from the PSD - if there are some obvious channels that have different / more noise components than the others
+* Also detect from looking at the raw plot - if there are some obvious channels that look much different to surrounding channels, (much more noise or are just flat)
+
+* Can also interpolate bad channels (for cross subject analysis to have same data dimentionality)
+    If need be interpolation can be done automatically - look here: https://autoreject.github.io/stable/index.html
+"""
+m_raw.plot() # Use this to inspect and select any bad channels
+m_raw.interpolate_bads() # This automatically interpolates the bad channels - and clears the 'bads' list after
+
+#-----------------------
+# TEMPORAL FILTERING
+#-----------------------
+"""
+Get a specific frequency band and remove non-relavant frequencies
+
+* Use IIR only when you want a steep cutoff (according to: https://mne.tools/stable/auto_tutorials/preprocessing/25_background_filtering.html
+* Use non-causal to not introduce delays
+"""
+# Remove low frequency drift
+m_raw = m_raw.filter(l_freq=0.1, h_freq=None) # Use 0.1 for evoked data
+
+#non-causal, Infinite Impulse Response (IIR) Butterworth filter of 2nd order
+m_filtered = m_raw.copy().filter(l_freq=0.1, h_freq=40) # Use 1hz for non evoked data
+alpha_band = (8, 14)# Not sure if we really need both of these - TODO: make sure only relevant things are kept (also do we do a band pass filter like this?)
+m_alpha = m_raw.copy().filter(l_freq=alpha_band[0], h_freq=alpha_band[1])
+
+
+#-----------------------
+# MARKING BAD SPANS
+#-----------------------
+"""
+This can be done manually or automatically
+This is really only needed if there are some specifically bad sections in the data - if they interfere with epochs they can be taken out then
 """
 
 #-----------------------
@@ -97,7 +141,56 @@ mne tutorial: https://mne.tools/stable/auto_tutorials/preprocessing/15_handling_
 do ica correction to remove artefacts
 do this on the baseline data and apply to the entire raw dataset
 """
+# ----- ICA ON BASELINE RAW DATA
+# High pass filter
+m_high = m_raw.copy()
+# Take out the first 10 secs - TODO: figure out if this is needed for everyone
+m_high.crop(tmin=10)
+m_high.filter(l_freq=1., h_freq=40)
+# get baseline data
+baseline_raw_data = df1.loc[df1['block_name'] == 'baseline']
+baseline_raw_start = baseline_raw_data['sample'].iloc[0] / fs
+baseline_raw_end = baseline_raw_data['sample'].iloc[-1] / fs
+baseline = m_high.copy()
+baseline.crop(tmin=baseline_raw_start, tmax=baseline_raw_end)
+# visualise the eog blinks
+# eog_evoked = create_eog_epochs(baseline, ch_name=['EOG', 'ECG']).average()
+# eog_evoked.apply_baseline(baseline=(None, -0.2))
+# eog_evoked.plot_joint()
+# do ICA
+baseline.drop_channels(['EOG', 'ECG'])
+ica = ICA(n_components=15, max_iter='auto', random_state=97)
+ica.fit(baseline)
+# Visualise
+m_high.load_data()
+ica.plot_sources(m_high, show_scrollbars=False)
+ica.plot_components()
+# Set ICA to exclued
+ica.exclude = [1, 11, 12, 13]  # ,14]
+reconst_raw = m_raw.copy()
+ica.apply(reconst_raw)
 
+
+#-----------------------
+# DOWN SAMPLING
+#-----------------------
+"""
+Down sample the data to 4* highest frequency after filtering
+Best practice - (to avoid issues with messing with timings and also issues with edge effects doing this after epoching)
+1. low-pass filter the Raw data at or below 1/3 of the desired sample rate, then
+2. decimate the data after epoching, by either passing the decim parameter to the Epochs constructor, or using the decimate() method after the Epochs have been created.
+
+-OR- (if can't do best practice) 
+Do this AFTER epoching (as it jitters the timings)
+"""
+# TODO: Should this be used instead of band pass filtering?
+# current_sfreq = m_raw.info['sfreq']
+# desired_sfreq = alpha_band[1]*4  # Hz
+# decim = np.round(current_sfreq / desired_sfreq).astype(int)
+# obtained_sfreq = current_sfreq / decim
+# lowpass_freq = obtained_sfreq / 3.
+#
+# raw_filtered = m_raw.copy().filter(l_freq=None, h_freq=lowpass_freq)
 
 #-----------------------
 # EPOCHING
@@ -109,13 +202,17 @@ for the nfb task - events are the following
     * visual probe in delay protocol (left and right)
 detect and eliminate bad epochs in this stage
 """
+## Epoch and downsample in one go
+# events = mne.find_events(raw_filtered)
+# epochs = mne.Epochs(raw_filtered, events, decim=decim)
+#
+# print('desired sampling frequency was {} Hz; decim factor of {} yielded an '
+#       'actual sampling frequency of {} Hz.'
+#       .format(desired_sfreq, decim, epochs.info['sfreq']))
 
+# Epoch all NFB trials (check what bagherzadeh does) - then average the source time course of these across participants
 
-#-----------------------
-# DOWN SAMPLING
-#-----------------------
-"""
-Down sample the data to 4* highest frequency
-Do this AFTER epoching (as it jitters the timings)
-"""
+# USE 'reject' to automatically reject bad epochs (over certain peak-peak amplitudes)
+#    reject_tmin and reject_tmax can also be used to determine the time frame to reject epochs (default whole epoch)
+
 
