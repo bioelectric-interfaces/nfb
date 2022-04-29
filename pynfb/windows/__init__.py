35bd92d1f3618cf9e655eea7b27f26628ed2b222
Chris <chrisjturner@gmail.com>
2022-04-29 12:25:06 +0100

    Make posnerCue same size as posnerFeedback

--- pynfb/windows/__init__.py
+++ pynfb/windows/__init__.py
@@ -14,7 +14,8 @@ from pynfb.helpers.dc_blocker import DCBlocker
 from pynfb.protocols import ParticipantInputProtocol, ParticipantChoiceProtocol, ExperimentStartProtocol, ImageProtocol, \
     EyeCalibrationProtocol
 from pynfb.protocols.widgets import ProtocolWidget, GaborFeedbackProtocolWidgetPainter, ParticipantChoiceWidgetPainter, \
-    PlotFeedbackWidgetPainter, PosnerFeedbackProtocolWidgetPainter, EyeTrackFeedbackProtocolWidgetPainter
+    PlotFeedbackWidgetPainter, PosnerFeedbackProtocolWidgetPainter, EyeTrackFeedbackProtocolWidgetPainter, \
+    PosnerCueProtocolWidgetPainter
 from pynfb.widgets.helpers import ch_names_to_2d_pos
 from pynfb.widgets.signals_painter import RawViewer
 from pynfb.widgets.topography import TopomapWidget
@@ -338,7 +339,7 @@ class SecondaryWindow(QtWidgets.QMainWindow):
             x_range = (screen.width()-margins.left()-margins.right())/2
             self.figure.setYRange(-y_range, y_range)
             self.figure.setXRange(-x_range, x_range)
-        elif isinstance(self.current_protocol.widget_painter, (PosnerFeedbackProtocolWidgetPainter, EyeTrackFeedbackProtocolWidgetPainter)):
+        elif isinstance(self.current_protocol.widget_painter, (PosnerFeedbackProtocolWidgetPainter, EyeTrackFeedbackProtocolWidgetPainter, PosnerCueProtocolWidgetPainter)):
             screen = QDesktopWidget().screenGeometry(self.figure)
             print(f"SCREEN H: {screen.height()}, SCREEN W: {screen.width()}")
             size = self.geometry()
