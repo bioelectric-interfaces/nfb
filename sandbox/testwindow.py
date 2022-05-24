50fa3d9fa2814fcdb97d095a2c857ad6bd2e13a9
Christopher John Turner (PGR) <2354158T@student.gla.ac.uk>
2022-05-24 18:46:28 +0100

    Create testwindow.py

---
+++ sandbox/testwindow.py
@@ -0,0 +1,48 @@
+# https://pythonprogramminglanguage.com/pyqt5-hello-world/
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize
+
+import logging
+import time
+
+class HelloWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(640, 480))
+        self.setWindowTitle("Hello world - pythonprogramminglanguage.com")
+
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        gridLayout = QGridLayout(self)
+        centralWidget.setLayout(gridLayout)
+
+        title = QLabel("Hello World from PyQt", self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        gridLayout.addWidget(title, 0, 0)
+
+    def keyPressEvent(self, e):
+        # TODO: make it so this can't be pressed when the pre-calcs are happening!!!!
+
+        if e.key() == QtCore.Qt.Key_Space:
+            # If the space key is pressed, then start the block
+            # Get the timestamp the key was pressed
+            # timestamp_str = datetime.strftime(datetime.now(), '%m-%d_%H-%M-%S')
+            self.key_press_time = int(time.time() * 1000)
+            print(f" SPACE PRESSED AT: {self.key_press_time}")
+
+        if e.key() == QtCore.Qt.Key_Escape:
+            # Toggle fullscreen on the escape key
+            if self.windowState() & QtCore.Qt.WindowFullScreen:
+                self.showNormal()
+            else:
+                self.showFullScreen()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
