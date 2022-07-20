5b2cdba82fe024a47a38826aef0a9acc54aeeca7
Chris <chrisjturner@gmail.com>
2022-07-20 18:13:29 +0100

    add CSP stuff (initial)

--- analysis/analysis_tests/covert_attn_test.py
+++ analysis/analysis_tests/covert_attn_test.py
@@ -26,6 +26,11 @@ from mne.minimum_norm import make_inverse_operator, apply_inverse_raw, apply_inv
 from pynfb.helpers import roi_spatial_filter as rsf
 from philistine.mne import savgol_iaf
 
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from mne.decoding import CSP
+
 if platform.system() == "Windows":
     userdir = "2354158T"
 else:
@@ -221,6 +226,39 @@ epochs = mne.Epochs(m_filt, events, event_id=event_dict, tmin=-2, tmax=7, baseli
 # epochs.drop([7,17,27,28,29]) # Drop bads for K's 2nd dataset
 
 fig = epochs.plot(events=events)
+
+###### CSP STUFF #############
+# Define a monte-carlo cross-validation generator (reduce variance):
+scores = []
+epochs_data = epochs.get_data()
+epochs_train = epochs.copy().crop(tmin=1., tmax=5.)
+labels = epochs.events[:, -1] - 2
+epochs_data_train = epochs_train.get_data()
+cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+cv_split = cv.split(epochs_data_train)
+
+# Assemble a classifier
+lda = LinearDiscriminantAnalysis()
+csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+
+# Use scikit-learn Pipeline with cross_val_score function
+clf = Pipeline([('CSP', csp), ('LDA', lda)])
+scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
+
+# Printing the results
+class_balance = np.mean(labels == labels[0])
+class_balance = max(class_balance, 1. - class_balance)
+print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores),
+                                                          class_balance))
+
+# plot CSP patterns estimated on full data for visualization
+csp.fit_transform(epochs_data, labels)
+
+csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
+#########################################
+
+
+
 
 probe_left = epochs['left_probe'].average()
 probe_right = epochs['right_probe'].average()
