7c3c7ab541c6f89262dd4f4d5eaae4da8d38db82
Chris <chrisjturner@gmail.com>
2022-09-18 23:14:21 +0100

    refactor and give structure to experiment and code

--- psychopy_tasks/posner_cvsa.py
+++ psychopy_tasks/posner_cvsa.py
@@ -9,221 +9,232 @@ from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 import psychopy
 import os
+import typing
+from dataclasses import dataclass
 
-TRIAL_REPS = [2,2,2,2]
-frameTolerance = 0.001  # how close to onset before 'same' frame
-expName = 'posner_task'
-### DIALOG BOX ROUTINE ###
-exp_info = {'participant': "99", 'session': 'x'}
-dlg = DlgFromDict(exp_info)
 
-exp_info['date'] = getDateStr()  # add a simple timestamp
-exp_info['expName'] = expName
-exp_info['psychopyVersion'] = psychopy.__version__
+@dataclass
+class PosnerComponent:
+    component: typing.Any = object()
+    start_time: float = 0.0
+    duration: float = 1.0
+    blocking: bool = False
 
-_thisDir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(_thisDir)
-filename = _thisDir + os.sep + u'data/%s_%s_%s' % (exp_info['participant'], expName, exp_info['date'])
-# An ExperimentHandler isn't essential but helps with data saving
-thisExp = ExperimentHandler(name=expName, version='',
-    extraInfo=exp_info, runtimeInfo=None,
-    originPath='C:\\Users\\2354158T\\Documents\\GitHub\\nfb\\psychopy\\posner_eyelink.py',
-    savePickle=True, saveWideText=True,
-    dataFileName=filename)
 
-# If pressed Cancel, abort!
-if not dlg.OK:
-    quit()
-else:
-    # Quit when either the participant nr or age is not filled in
-    if not exp_info['participant'] or not exp_info['session']:
+class PosnerTask:
+    def __init__(self):
+        self.trial_reps = [4, 2, 2, 2]
+        self.frameTolerance = 0.001  # how close to onset before 'same' frame
+        self.expName = 'posner_task'
+        self.exp_info = {'participant': "99", 'session': 'x'}
+        self.thisExp = None
+        # init the monitor
+        self.mon = Monitor('eprime',
+                           width=40,
+                           distance=60,
+                           autoLog=True)
+        self.mon.setSizePix((1280, 1024))
+        self.win = Window(fullscr=False, monitor=self.mon)
+        self.start_components = []
+        self.trial_components = []
+        self.continue_components = []
+        self.end_components = []
+
+        # Initialize clocks
+        self.global_clock = Clock()
+        self.trial_clock = Clock()
+
+    def update_exp_info(self):
+        self.exp_info['date'] = getDateStr()  # add a simple timestamp
+        self.exp_info['expName'] = self.expName
+        self.exp_info['psychopyVersion'] = psychopy.__version__
+
+    def set_experiment(self):
+        _thisDir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(_thisDir)
+        filename = _thisDir + os.sep + u'data/%s_%s_%s' % (
+            self.exp_info['participant'], self.expName, self.exp_info['date'])
+        # An ExperimentHandler isn't essential but helps with data saving
+        self.thisExp = ExperimentHandler(name=self.expName, version='',
+                                         extraInfo=self.exp_info, runtimeInfo=None,
+                                         originPath='C:\\Users\\2354158T\\Documents\\GitHub\\nfb\\psychopy\\posner_eyelink.py',
+                                         savePickle=True, saveWideText=True,
+                                         dataFileName=filename)
+
+    def init_components(self):
+        self.kb = Keyboard()  # Keyboard to use throughout
+        self.start_text = PosnerComponent(
+            TextStim(self.win, text="""Welcome to this experiment!
+                                                 Press SPACE to start"""),
+            duration=0.0,
+            blocking=True)
+        self.start_components = [self.start_text]
+
+        self.fc = PosnerComponent(
+            circle.Circle(
+                win=self.win,
+                name='fc',
+                units="deg",
+                radius=0.1,
+                fillColor='black',
+                lineColor='black'
+            ),
+            duration=1.0,
+            start_time=0.0)
+
+        self.left_probe = PosnerComponent(
+            circle.Circle(
+                win=self.win,
+                name='left_probe',
+                units="deg",
+                radius=3.5,
+                fillColor='blue',
+                lineColor='white',
+                lineWidth=8,
+                edges=128,
+                pos=[-5, -1]
+            ),
+            duration=2.0,
+            start_time=0.0)
+
+        self.right_probe = PosnerComponent(
+            circle.Circle(
+                win=self.win,
+                name='right_probe',
+                units="deg",
+                radius=3.5,
+                fillColor='blue',
+                lineColor='white',
+                lineWidth=8,
+                edges=256,
+                pos=[5, -1]
+            ),
+            duration=2.0,
+            start_time=1.0)
+
+        self.trial_components = [self.fc, self.left_probe, self.right_probe]
+
+    def handle_component(self, pcomp, tThisFlip, tThisFlipGlobal, t, start_time=0, duration=1):
+        # Handle both the probes
+        if pcomp.component.status == NOT_STARTED and tThisFlip >= start_time - self.frameTolerance:
+            # keep track of start time/frame for later
+            pcomp.component.tStart = t  # local t and not account for scr refresh
+            pcomp.component.tStartRefresh = tThisFlipGlobal  # on global time
+            self.win.timeOnFlip(pcomp.component, 'tStartRefresh')  # time at next scr refresh
+            # add timestamp to datafile
+            self.thisExp.timestampOnFlip(self.win, f'{pcomp.component.name}.started')
+            pcomp.component.setAutoDraw(True)
+        if pcomp.component.status == STARTED:
+            # is it time to stop? (based on global clock, using actual start)
+            if tThisFlipGlobal > pcomp.component.tStartRefresh + duration - self.frameTolerance:
+                if not pcomp.blocking:
+                    pcomp.component.tStop = t  # not accounting for scr refresh
+                    # add timestamp to datafile
+                    self.thisExp.timestampOnFlip(self.win, f'{pcomp.component.name}.stopped')
+                    pcomp.component.setAutoDraw(False)
+                    pcomp.component.status = FINISHED
+
+    def run_block(self, component_list, trial_reps, name='block'):
+        trials = TrialHandler(nReps=trial_reps, method='sequential',
+                              extraInfo=self.exp_info, originPath=-1,
+                              trialList=[None],
+                              seed=None, name='trials')
+        self.thisExp.addLoop(trials)  # add the loop to the experiment
+        thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
+
+        # Do the trials
+        for trial_index, thisTrial in enumerate(trials):
+            print(f'STARTING TRIAL: {trials.thisN} OF BLOCK: {name}')
+            currentLoop = trials
+            # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+            if thisTrial != None:
+                for paramName in thisTrial:
+                    exec('{} = thisTrial[paramName]'.format(paramName))
+
+            continueRoutine = True
+
+            # Reset the trial clock
+            self.trial_clock.reset()
+
+            for thisComponent in component_list:
+                thisComponent.component.tStart = None
+                thisComponent.component.tStop = None
+                thisComponent.component.tStartRefresh = None
+                thisComponent.component.tStopRefresh = None
+                if hasattr(thisComponent.component, 'status'):
+                    thisComponent.component.status = NOT_STARTED
+
+            while continueRoutine:
+                # get current time
+                t = self.trial_clock.getTime()
+                tThisFlip = self.win.getFutureFlipTime(clock=self.trial_clock)
+                tThisFlipGlobal = self.win.getFutureFlipTime(clock=None)
+
+                # Handle both the probes
+                for thisComponent in component_list:
+                    self.handle_component(thisComponent, tThisFlip, tThisFlipGlobal, t, start_time=0,
+                                          duration=thisComponent.duration)
+                    # check for blocking end (typically the Space key)
+                    if self.kb.getKeys(keyList=["space"]):
+                        thisComponent.blocking = False
+
+                # check for quit (typically the Esc key)
+                if self.kb.getKeys(keyList=["escape"]):
+                    quit()
+
+                continueRoutine = False  # will revert to True if at least one component still running
+                for thisComponent in component_list:
+                    if hasattr(thisComponent.component, "status") and thisComponent.component.status != FINISHED:
+                        continueRoutine = True
+                        break  # at least one component has not yet finished
+
+                # refresh the screen
+                if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                    self.win.flip()
+
+            # --- Ending Routine "cue" ---
+            for thisComponent in component_list:
+                if hasattr(thisComponent.component, "setAutoDraw"):
+                    thisComponent.component.setAutoDraw(False)
+            self.thisExp.nextEntry()
+
+    def run_start_block(self):
+        pass
+
+    def run_continue_block(self):
+        pass
+
+    def run_end_block(self):
+        pass
+
+    def show_start_dialog(self):
+        dlg = DlgFromDict(self.exp_info)
+        # If pressed Cancel, abort!
+        if not dlg.OK:
+            quit()
+        else:
+            # Quit when either the participant nr or age is not filled in
+            if not self.exp_info['participant'] or not self.exp_info['session']:
+                quit()
+
+            else:  # let's star the experiment!
+                print(f"Started experiment for participant {self.exp_info['participant']} "
+                      f"in session {self.exp_info['session']}.")
+
+    def end_experiment(self):
+        # Finish experiment by closing window and quitting
+        self.win.close()
         quit()
 
-    else:  # let's star the experiment!
-        print(f"Started experiment for participant {exp_info['participant']} "
-              f"in session {exp_info['session']}.")
+    def run_experiment(self):
+        self.show_start_dialog()
+        self.update_exp_info()
+        self.set_experiment()
+        self.init_components()
+        self.run_block(self.start_components, 1, name='start')
+        self.run_block(self.trial_components, self.trial_reps[0], name='trials1')
 
-# init the monitor
-mon = Monitor('eprime',
-                 width=40,
-                 distance=60,
-                 autoLog=True)
-mon.setSizePix((1280, 1024))
 
-# Initialize a fullscreen window with my monitor (HD format) size
-# and my monitor specification called "samsung" from the monitor center
-win = Window(fullscr=False, monitor=mon)
-
-# Also initialize a mouse, for later
-# We'll set it to invisible for now
-mouse = Mouse(visible=False)
-
-# Initialize clocks
-clock = Clock() #global clock
-trial_clock = Clock()
-
-# Initialize Keyboard
-kb = Keyboard()
-
-# Init the start screen components
-welcome_txt_stim = TextStim(win, text="""Welcome to this experiment!
-                                         Press SPACE to start""")
-welcome_txt_stim.setAutoDraw(True)
-
-# Init the trial components
-fc = circle.Circle(
-    win=win,
-    name='fc',
-    units="deg",
-    radius=0.1,
-    fillColor='black',
-    lineColor='black'
-)
-
-left_probe = circle.Circle(
-    win=win,
-    name='left_probe',
-    units="deg",
-    radius=3.5,
-    fillColor='blue',
-    lineColor='white',
-    lineWidth=8,
-    edges=128,
-    pos=[-5, -1]
-)
-right_probe = circle.Circle(
-    win=win,
-    name='right_probe',
-    units="deg",
-    radius=3.5,
-    fillColor='blue',
-    lineColor='white',
-    lineWidth=8,
-    edges=256,
-    pos=[5, -1]
-)
-
-### START BODY OF EXPERIMENT ###
-#
-# Do the init screen
-win.flip()
-# Wait until space is pressed before moving on
-while True:
-    keys = kb.getKeys()
-    if "space" in keys:
-        break
-
-trials_1 = TrialHandler(nReps=TRIAL_REPS[0], method='sequential',
-    extraInfo=exp_info, originPath=-1,
-    trialList=[None],
-    seed=None, name='trials')
-thisExp.addLoop(trials_1)  # add the loop to the experiment
-thisTrial = trials_1.trialList[0]  # so we can initialise stimuli with some values
-
-# Do the trials
-for trial_index, thisTrial in enumerate(trials_1):
-    print(f'STARTING TRIAL: {trials_1.thisN}')
-    currentLoop = trials_1
-    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
-    if thisTrial != None:
-        for paramName in thisTrial:
-            exec('{} = thisTrial[paramName]'.format(paramName))
-
-    continueRoutine = True
-    cueComponents = [fc, left_probe, right_probe]
-
-    # Reset the trial clock
-    trial_clock.reset()
-
-    for thisComponent in cueComponents:
-        thisComponent.tStart = None
-        thisComponent.tStop = None
-        thisComponent.tStartRefresh = None
-        thisComponent.tStopRefresh = None
-        if hasattr(thisComponent, 'status'):
-            thisComponent.status = NOT_STARTED
-
-    while continueRoutine:
-        # get current time
-        t = trial_clock.getTime()
-        tThisFlip = win.getFutureFlipTime(clock=trial_clock)
-        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-
-        # Handle both the probes
-        if left_probe.status == NOT_STARTED and tThisFlip >= 0-frameTolerance:
-            # keep track of start time/frame for later
-            left_probe.tStart = t  # local t and not account for scr refresh
-            left_probe.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(left_probe, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'left_probe.started')
-            left_probe.setAutoDraw(True)
-        if left_probe.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > left_probe.tStartRefresh + 6.6-frameTolerance:
-                left_probe.tStop = t  # not accounting for scr refresh
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'probe_l_cue.stopped')
-                left_probe.setAutoDraw(False)
-                left_probe.status = FINISHED
-
-        if right_probe.status == NOT_STARTED and tThisFlip >= 0-frameTolerance:
-            # keep track of start time/frame for later
-            right_probe.tStart = t  # local t and not account for scr refresh
-            right_probe.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(right_probe, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'right_probe.started')
-            right_probe.setAutoDraw(True)
-        if right_probe.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > right_probe.tStartRefresh + 6.6-frameTolerance:
-                right_probe.tStop = t  # not accounting for scr refresh
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'right_probe.stopped')
-                right_probe.setAutoDraw(False)
-                right_probe.status = FINISHED
-
-        # Handle the fixation circle
-        if fc.status == NOT_STARTED and tThisFlip >= 0-frameTolerance:
-            # keep track of start time/frame for later
-            fc.tStart = t  # local t and not account for scr refresh
-            fc.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(fc, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'fc.started')
-            fc.setAutoDraw(True)
-        if fc.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > fc.tStartRefresh + 1.0-frameTolerance:
-                fc.tStop = t  # not accounting for scr refresh
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'fc.stopped')
-                fc.setAutoDraw(False)
-                fc.status = FINISHED
-
-        # check for quit (typically the Esc key)
-        if kb.getKeys(keyList=["escape"]):
-            quit()
-
-        continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in cueComponents:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
-
-        # refresh the screen
-        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-            win.flip()
-
-    # --- Ending Routine "cue" ---
-    for thisComponent in cueComponents:
-        if hasattr(thisComponent, "setAutoDraw"):
-            thisComponent.setAutoDraw(False)
-    thisExp.nextEntry()
-
-### END BODY OF EXPERIMENT ###
-
-# Finish experiment by closing window and quitting
-win.close()
-quit()
+if __name__ == '__main__':
+    pt = PosnerTask()
+    pt.run_experiment()
