8151a5e8deedb319820fba5207f3635d40380dbd
chrisjturner <chrisjturner@gmail.com>
2022-09-21 12:25:03 +0100

    Update check_asc_file.py

--- analysis/analysis_tests/check_asc_file.py
+++ analysis/analysis_tests/check_asc_file.py
@@ -1,6 +1,9 @@
 import re
+import pandas as pd
+from pprint import pprint
 
 ascFile = r"C:\Users\Chris\Documents\GitHub\nfb\psychopy_tasks\eye_track_data\99_psnr_2022_09_19_19_18.asc"
+ascFile = r'C:\Users\Chris\Documents\GitHub\nfb\psychopy_tasks\eye_track_data\99_psnr_2022_09_21_11_02.asc'
 
 file1 = open(ascFile, 'r')
 Lines = file1.readlines()
@@ -12,54 +15,41 @@ fc_start = 0
 
 first = False
 offset = 0
-
-# def parse_component(line, component_name, offset):
-#     start = 0
-#     end = 0
-#     id = 0
-#     if 'fc' in line and 'START' in line:
-#         start = int(line.split(' ')[0].split('\t')[1])
-#     elif 'fc' in line and 'END' in line:
-#         end = int(line.split(' ')[0].split('\t')[1])
-#         length = int(end) - int(start)
-#         print(f'{id} fc start: {start - offset}, end: {end - offset}, length: {length}')
-
 count = 0
+asc_data = {}
+component_list = ['fc',
+                  'left_probe',
+                  'right_probe',
+                  'left_cue',
+                  'right_cue',
+                  'centre_cue1',
+                  'centre_cue2',
+                  'stim',
+                  'key_resp']
 # Strips the newline character
 for line in Lines:
     count += 1
-    if not first and 'MSG' in line:
-        print("Line{}: {}".format(count, line.strip()))
-        offset = int(line.split()[1])
-        first = True
     if 'TRIAL' in line:
-        # print("Line{}: {}".format(count, line.strip()))
         if 'trials' in line and 'START' in line:
             trial_start = int(line.split(' ')[0].split('\t')[1])
-            trial_id = line.split(' ')[1].split('_')[2]
+            trial_id = line.split(' ')[1].split('_')[1]
+            asc_data[trial_id] = {}
             # print("Line{}: {}".format(count, line.strip()))
-        elif 'trials' in line and 'END' in line:
-            trial_end = int(line.split(' ')[0].split('\t')[1])
-            trial_length = int(trial_end) - int(trial_start)
-            print(f'{trial_id} trials start: {trial_start-offset}, end: {trial_end-offset},  length: {trial_length}')
+        if 'START' in line:
+            for cmp in component_list:
+                if cmp in line:
+                    asc_data[trial_id][cmp] = {}
+                    start = int(line.split(' ')[0].split('\t')[1])
+                    asc_data[trial_id][cmp]['start'] = start
+        if 'END' in line:
+            for cmp in component_list:
+                if cmp in line:
+                    end = int(line.split(' ')[0].split('\t')[1])
+                    asc_data[trial_id][cmp]['end'] = end
+                    asc_data[trial_id][cmp]['length'] = asc_data[trial_id][cmp]['end'] - asc_data[trial_id][cmp]['start']
+pprint(asc_data)
 
-        if 'fc' in line and 'START' in line:
-            fc_start = int(line.split(' ')[0].split('\t')[1])
-        elif 'fc' in line and 'END' in line:
-            fc_end = int(line.split(' ')[0].split('\t')[1])
-            fc_length = int(fc_end) - int(fc_start)
-            print(f'{trial_id} fc start: {fc_start-offset}, end: {fc_end-offset}, length: {fc_length}')
-
-        if 'left_cue' in line and 'START' in line:
-            left_cue_start = int(line.split(' ')[0].split('\t')[1])
-        elif 'left_cue' in line and 'END' in line:
-            left_cue_end = int(line.split(' ')[0].split('\t')[1])
-            left_cue_length = int(left_cue_end) - int(left_cue_start)
-            print(f'{trial_id} left_cue start: {left_cue_start-offset}, end: {left_cue_end-offset}, length: {left_cue_length}')
-
-        if 'stim' in line and 'START' in line:
-            stim_start = int(line.split(' ')[0].split('\t')[1])
-        elif 'stim' in line and 'END' in line:
-            stim_end = int(line.split(' ')[0].split('\t')[1])
-            stim_length = int(stim_end) - int(stim_start)
-            print(f'{trial_id} stim start: {stim_start-offset}, end: {stim_end-offset}, length: {stim_length}')
+key = 'start'
+for k, v in asc_data.items():
+    for kk, vv in v.items():
+        print(f"{k}: {kk}: {key}: {vv[key]}")
